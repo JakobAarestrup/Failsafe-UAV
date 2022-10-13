1ec7b98b60a1c45020f6aef1de26ceed067890d6
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-10-13 17:27:41 +0200

    Update i2ctest2.cpp

--- IMU/i2ctest2.cpp
+++ IMU/i2ctest2.cpp
@@ -13,6 +13,19 @@
 #define REG_DATA_Z_LOW  0x36
 #define REG_DATA_Z_HIGH 0x37
 
+#define LSM6DSOX_FUNC_CFG_ACCESS 0x1 ///< Enable embedded functions register
+#define LSM6DSOX_PIN_CTRL 0x2        ///< Pin control register
+
+#define LSM6DSOX_INT1_CTRL 0x0D ///< Interrupt enable for data ready
+#define LSM6DSOX_CTRL1_XL 0x10  ///< Main accelerometer config register
+#define LSM6DSOX_CTRL2_G 0x11   ///< Main gyro config register
+#define LSM6DSOX_CTRL3_C 0x12   ///< Main configuration register
+#define LSM6DSOX_CTRL9_XL 0x18  ///< Includes i3c disable bit
+
+void enableI2CMasterPullups(bool enable_pullups);
+void disableSPIMasterPullups(bool disable_pullups);
+int fd;
+
 int main (int argc, char **argv)
 {
     // Setup I2C communication
@@ -22,6 +35,8 @@ int main (int argc, char **argv)
         return -1;
     }
     std::cout << "I2C communication successfully setup.\n";
+
+    int wiringPiI2CRead(fd);
 
     return 0;
 }
