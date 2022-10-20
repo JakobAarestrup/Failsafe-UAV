2c62eb2961ae1743a072c4ffdb033e29d23c071d
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-20 14:46:51 +0200

    Update GPS.cpp

---
+++ GPS/GPS.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <wiringSerial.h>
+
+int main() {
+  int fd;
+  if ((fd = serialOpen("/dev/ttyAMA0",9600))<0) {
+    fprintf(stderr,"FAIL");
+    return 1;
+  }
+
+  for (;;) {
+    putchar(serialGetchar(fd));
+    fflush (stdout) ;
+  }
+  retunr 0;
+}
