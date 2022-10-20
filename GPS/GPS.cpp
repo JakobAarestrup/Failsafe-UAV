0d2f82fd9eecf6c01bc57fcf9f51c67214cc9d2a
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-20 15:17:22 +0200

    Update GPS.cpp

--- GPS/GPS.cpp
+++ GPS/GPS.cpp
@@ -6,15 +6,22 @@
 
 int main() {
   int fd;
-  if ((fd = serialOpen("/dev/ttyAMA0",9600))<0) {
+
+  if ((fd = serialOpen("/dev/ttyAMA0",9600))<0) 
+  {
     printf("FAIL");
     return 0;
   }
-    printf("SUCCESS\n");
-  while(1){
+
+  printf("SUCCESS\n");
+
+  while(1)
+  {
     printf("%d\n",serialGetchar(fd));
     fflush(stdout);
+
     sleep(1);
   }
+
   return 0;
 }
