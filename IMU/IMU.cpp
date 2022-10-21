64b7daa8e9523a50df361437f1afbd9a04f09401
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-10-21 15:04:45 +0200

    Update IMU.cpp

--- IMU/IMU.cpp
+++ IMU/IMU.cpp
@@ -22,19 +22,19 @@ void IMU::ConvertIMUData(int x, int y, int z, int sensor_handle)
         double x_ACC = float(x);
         double y_ACC = float(y);
         double z_ACC = float(z);
-        this.roll_ = atan2(y_ACC , z_ACC) * 57.3;
-        this.pitch_ = atan2((- x_ACC), sqrt(y_ACC * y_ACC + z_ACC * z_ACC)) * 57.3;
+        this->roll_ = atan2(y_ACC , z_ACC) * 57.3;
+        this->pitch_ = atan2((- x_ACC), sqrt(y_ACC * y_ACC + z_ACC * z_ACC)) * 57.3;
     }
     else if (sensor_handle == 2) // Gyroscope
     {
-        this.rate_gyr_x_ = float(x) * G_GAIN;
-	    this.rate_gyr_y_ = float(y) * G_GAIN;
-	    this.rate_gyr_z_ = float(z) * G_GAIN;
+        this->rate_gyr_x_ = float(x) * G_GAIN;
+	    this->rate_gyr_y_ = float(y) * G_GAIN;
+	    this->rate_gyr_z_ = float(z) * G_GAIN;
 
         //Calculate the angles from the gyro
-	    this.gyr_Xangle_ = this.rate_gyr_x_*DT;
-	    this.gyr_Yangle_ = this.rate_gyr_y_*DT;
-	    this.gyr_Zangle_ = this.rate_gyr_z_*DT;
+	    this->gyr_Xangle_ = this->rate_gyr_x_*DT;
+	    this->gyr_Yangle_ = this->rate_gyr_y_*DT;
+	    this->gyr_Zangle_ = this->rate_gyr_z_*DT;
     }
     else if (sensor_handle == 3) // Magnetometer
     {
