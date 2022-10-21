55833e8a2374ef0ad09b6bc9eacdb179d0cab838
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-10-21 14:01:09 +0200

    general change in IMU and i2c

--- IMU/I2C.hpp
+++ IMU/I2C.hpp
@@ -5,8 +5,8 @@ class I2C
 public:
     I2C();
     void openI2C(int ADDR);
-    void WriteI2C(int ADDR, int reg, int data);
-    int ReadI2C(int fd, int ADDR, int reg);
+    void WriteI2C(int reg, int data);
+    int ReadI2C(int ADDR, int reg);
     double ConvertI2CData(int x, int y, int z);
     ~I2C();
 private:
