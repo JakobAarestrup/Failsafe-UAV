e661db75d89820f7237dd0dc377740339032d7cf
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-10-24 15:11:22 +0200

    Update main.cpp

--- IMU/main.cpp
+++ IMU/main.cpp
@@ -80,7 +80,7 @@ I1.WriteI2C(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b10101100);
     int MAG_Y = IMU2.getMagY();
     int MAG_Z = IMU2.getMagZ();
 
-    printf("MAG_X: %f \nMAG_Y: %f \nMAG_Z: %f \n", MAG_X, MAG_Y, MAG_Z); //Print mag data
+    printf("MAG_X: %f \nMAG_Y: %f \nMAG_Z: %f \n", MAG_x, MAG_y, MAG_z); //Print mag data
 
     usleep(1000000); //Small delay
 }
