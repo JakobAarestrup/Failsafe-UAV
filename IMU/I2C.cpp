c03a4e60f56432240e6929b6971936bc68619380
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-10-24 15:09:56 +0200

    Update I2C.cpp

--- IMU/I2C.cpp
+++ IMU/I2C.cpp
@@ -55,7 +55,7 @@ int I2C::ReadI2C(int ADDR, int reg)
     }
     else
    {
-        I2CData_ = (Reg_data2 << 8) | Reg_data1;
-        return I2CData_;
+        I2C_Data_ = (Reg_data2 << 8) | Reg_data1;
+        return I2C_Data_;
    }
 }
