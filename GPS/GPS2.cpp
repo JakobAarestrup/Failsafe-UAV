c42e9bfcfc2b330576e3e8fdd576330846c4d72a
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-24 10:10:41 +0200

    ny gps kode og python eksempel

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+
+#include <wiringPi.h>
 #include <wiringSerial.h>
 
-int main() {
-  int fd;
-  if ((fd = serialOpen("/dev/serial0",9600))<0) {
-    fprintf(stderr,"FAIL\n");
-    return 1;
+int main ()
+{
+  int serial_port ;
+  char dat;
+  if ((serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)	/* open serial port */
+  {
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+    return 1 ;
   }
-  for (;;) {
-    printf("%d",serialGetchar(fd));
-    fflush (stdout) ;
+
+  if (wiringPiSetup () == -1)					/* initializes wiringPi setup */
+  {
+    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+    return 1 ;
   }
+
+  while(1){
+	  
+	if(serialDataAvail (serial_port) )
+	{ 
+		dat = serialGetchar (serial_port);		/* receive character serially*/	
+		printf ("%c", dat) ;
+		fflush (stdout) ;
+		serialPutchar(serial_port, dat);		/* transmit character serially on port */
+		  }
+	}
+
 }
