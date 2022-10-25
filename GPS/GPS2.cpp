0a899b8ded7ad5d1be0f0ab73575d565dec387c6
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-25 15:18:00 +0200

    Update GPS2.cpp

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -1,66 +1,45 @@
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <iostream>
-#include <unistd.h>
-
-#include <wiringPi.h>
-#include <wiringSerial.h>
-
-int main ()
+#include<stdio.h>
+#include<errno.h>
+#include<wiringPi.h>
+#include<string.h>
+#include<wiringSerial.h>
+int main()
 {
-  int serial_port ;
-  char data;
-  int flag = 0;
-  char gps[65];
-  char arr[]="$GPGGA";
-  if ((serial_port = serialOpen ("/dev/ttyS0", 9600 )) < 0)	/* open serial port */
-  {
-    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
-    return 1 ;
-  }
-
-  if (wiringPiSetup () == -1)					/* initializes wiringPi setup */
-  {
-    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
-    return 1 ;
-  }
-
-  while(1){
-	int i = 0; 
-  int c;
-	if(serialDataAvail (serial_port))
-	{ 
-		data = serialGetchar (serial_port);		// receive character serially
-    // kig efter GPGGA
-    if(c=serialGetchar(serial_port)==13||10)
-    {
-      for(i=0; i<6 ;i++)
-      {
-        if(serialGetchar(serial_port)==arr[i])
-          {
-            flag++;
-          }
-            if (flag == 5)
-              {
-                flag = 0;
-              }
-      }
-    }
-
-    if(flag==6)
-    {
-      flag=0;
-        for(i=0;i<=65;i++)
-        gps[i]=serialGetchar(serial_port);
-    }
-   
-    printf("value of b_static: %.*s\n", (int)sizeof(gps), gps);
-		//serialPutchar(serial_port, dat); // transmit character serially on port
-  
-    usleep(1000000);
-  }
-
-	}
-
+char gps[65];   //gps string array
+int fd,flag=0;    //handler
+char arr[]="$GPGGA";
+printf("raspberry gps programme");
+if(wiringPiSetup()<0)     //setup wiringpi
+return -1;
+else
+printf("setup is ok\n");
+if((fd=serialOpen("/dev/ttyS0",9600))<0)
+{
+fprintf(stderr,"unable to open serial device%s\n",strerror(errno));
 }
+else
+{
+printf("serial UART is Ok\n");
+}
+while(1)
+{
+int i=0;
+int c;
+if(c=serialGetchar(fd)==13||10)
+{
+for(i=0;i<6;i++)
+{
+if(serialGetchar(fd)==arr[i])
+flag++;
+}
+}
+if(flag==6)
+{
+flag=0;
+for(i=0;i<=65;i++)
+gps[i]=serialGetchar(fd);
+}
+
+printf("%d",gps);
+}
+fflush(stdout);
