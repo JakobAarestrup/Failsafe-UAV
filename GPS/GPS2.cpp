447a648afac16fc117e5c68d8f26cd86bcc32cf9
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-27 15:29:13 +0200

    Update GPS2.cpp

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -94,6 +94,7 @@ int main ()
       longitude_  = atof(NMEA[4]);
       satellites_ = atoi(NMEA[7]);
       printf("GGA: %s\n",buff);
+      usleep(1000000);
       printf("NMEA: Latitude: %f %s Longitude: %f %s SV: %d\n",latitude_,Pole_NS,longitude_,Pole_WE,satellites_);
       i = 0; // reset i variable
       count = 0; 
