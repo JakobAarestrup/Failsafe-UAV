37292e079893a44decfc7902a322a09deb16588a
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-27 13:25:45 +0200

    Update GPS2.cpp

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -72,9 +72,9 @@ int main ()
       char* satellites = NMEA[7];
 
       // conversion
-      float latitude_  = std::atof(latitude);
-      float longitude_ = std::atof(longitude);
-      int satellites_  = std::atoi(satellites);
+      float latitude_  = std::stof(latitude);
+      float longitude_ = std::stof(longitude);
+      int satellites_  = std::stoi(satellites);
 
       printf("NMEA: Latitude: %f %s Longitude: %f %s SV: %d",latitude_,Pole_NS,longitude_,Pole_WE,satellites_);
       count = 0; // reset count
