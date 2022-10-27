fb3830292d04f6d8e4ec90741d7f65c36dc10296
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-27 14:45:00 +0200

    Update GPS2.cpp

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -86,7 +86,7 @@ int main ()
       //char* longitude = NMEA[4];
       char* Pole_NS = NMEA[3];
       char* Pole_WE = NMEA[5];
-      printf("Pole_WE %s",Pole_WE);       
+      //printf("Pole_WE %s",Pole_WE);       
       /* latitude_   = atof(NMEA[2]);
       longitude_  = atof(NMEA[4]);
       satellites_ = atoi(NMEA[7]);
