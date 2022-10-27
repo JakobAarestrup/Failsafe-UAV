4c6713af740261b3ff93b9c6c18e4ca1167aa486
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-27 10:42:57 +0200

    Updated

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -1,69 +1,60 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <iostream>
-#include <unistd.h>
 
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
 int main ()
 {
-  int serial_port ;
-  char data;
-  int flag = 0;
-  char gps[65];
-  char arr[]={'G','P','G','G','A','\0'};
-  if ((serial_port = serialOpen ("/dev/ttyS0", 9600 )) < 0)	/* open serial port */
+  int serial_port; 
+  char dat,buff[100],GGA_code[3];
+  unsigned char IsitGGAstring=0;
+  unsigned char GGA_index=0;
+  unsigned char is_GGA_received_completely = 0;
+  
+  if ((serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)		/* open serial port */
   {
     fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
-  if (wiringPiSetup () == -1)					/* initializes wiringPi setup */
+  if (wiringPiSetup () == -1)							/* initializes wiringPi setup */
   {
     fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
     return 1 ;
   }
 
-  while(1)
-  {
-
-    int i = 0; 
-    char c;
-
-    if(serialDataAvail (serial_port))
-    { 
-      //data = serialGetchar (serial_port);		// receive character serially
-      // kig efter GPGGA
-      if(c=serialGetchar(serial_port)=='G')
-      {
-        for(i=0; i<6 ;i++)
-        {
-          if(serialGetchar(serial_port)==arr[i])
-            {
-              flag++;
-            }
-              if (flag == 5)
-                {
-                  flag = 0;
-                }
-        }
-      }
-
-      if(flag==6)
-      {
-        flag=0;
-          for(i=0;i<=65;i++)
-          gps[i]=serialGetchar(serial_port);
-      }
-    
-      printf("Value of array: %.*s\n", (int)sizeof(gps), gps);
-      //serialPutchar(serial_port, dat); // transmit character serially on port
-    
-      usleep(1000000);
-    }
-
+  while(1){
+	  
+		if(serialDataAvail (serial_port) )		/* check for any data available on serial port */
+		  { 
+			dat = serialGetchar(serial_port);		/* receive character serially */		
+			if(dat == '$'){
+				IsitGGAstring = 0;
+				GGA_index = 0;
+			}
+			else if(IsitGGAstring ==1){
+				buff[GGA_index++] = dat;
+				if(dat=='\r')
+					is_GGA_received_completely = 1;
+				}
+			else if(GGA_code[0]=='G' && GGA_code[1]=='G' && GGA_code[2]=='A'){
+				IsitGGAstring = 1;
+				GGA_code[0]= 0; 
+				GGA_code[0]= 0;
+				GGA_code[0]= 0;		
+				}
+			else{
+				GGA_code[0] = GGA_code[1];
+				GGA_code[1] = GGA_code[2];
+				GGA_code[2] = dat;
+				}
+		  }
+		if(is_GGA_received_completely==1){
+			printf("GGA: %s",buff);
+			is_GGA_received_completely = 0;
+		}
 	}
-
+	return 0;
 }
