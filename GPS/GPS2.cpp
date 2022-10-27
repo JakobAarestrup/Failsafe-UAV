b18dc92812752b45142bf9e77152b15472c1a9d8
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-27 13:16:01 +0200

    Made variable from ascii string code

--- GPS/GPS2.cpp
+++ GPS/GPS2.cpp
@@ -13,6 +13,8 @@ int main ()
   unsigned char GGA_index=0;
   unsigned char is_GGA_received_completely = 0;
   int length = 0;
+  int count = 0;
+
   
   if ((serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)		/* open serial port */
   {
@@ -54,9 +56,29 @@ int main ()
 				}
 		  }
 		if(is_GGA_received_completely==1){
-			printf("GGA: %s",buff);
-      length = sizeof(buff);
-      printf("Length: %d",length);
+			//printf("GPGGA: %s",buff);
+      char* NMEA[15];
+      char* b1 = strtok(buff,","); // first token
+      while (b1 != NULL)
+      {
+          NMEA[count++] = b1; 
+          b1 = strtok(NULL, ",");
+          
+      }
+      char* latitude = NMEA[2];
+      char* longitude = NMEA[4];
+      char* Pole_NS = NMEA[3];
+      char* Pole_WE = NMEA[5];        
+      char* satellites = NMEA[7];
+
+      // conversion
+      float latitude_ = atof(latitude);
+      float longitude_ = atof(longitude);
+      int satellites_ = atoi(satellites);
+
+      printf("NMEA: %f %s %f %s %d",latitude_,Pole_NS,longitude_,Pole_WE,satellites_);
+      char* b1 = 0;
+      count = 0;
 			is_GGA_received_completely = 0;
 		}
 	}
--- Random/test.cpp
+++ Random/test.cpp
@@ -9,32 +9,36 @@ using namespace std;
 
 int main ()
 {
-  int serial_port ;
-  char data;
-  int flag = 0;
-  char gps[65];
-  char arr[]={'G','P','G','G','A','\0'};
- 
-  while(1)
-  {
+    int count = 0;
+    char buff[] = "GPGGA,134658.00,5106.9792,N,11402.3003,W,2,09,1.0,1048.47,M,-16.27,M,08,AAAA*60";
+    char* NMEA[15];
+   /*  char* latitude = strtok(NULL, ","); // third token
+    char* Pole_NS = strtok(NULL, ","); // fourth token
+    char* longitude = strtok(NULL, ","); // fifth token
+    char* Pole_WS = strtok(NULL, ","); // sixth token
+    char* token_2 = strtok(NULL, ","); // seventh token
+    char* satellites = strtok(NULL, ","); // eight token */
+    char* b1 = strtok(buff,","); // first token
+    while (b1 != NULL)
+    {
+        NMEA[count++] = b1; 
+        b1 = strtok(NULL, ",");
+        
+    }
+    char* latitude = NMEA[2];
+    char* longitude = NMEA[4];
+    char* Pole_NS = NMEA[3];
+    char* Pole_WE = NMEA[5];        
+    char* satellites = NMEA[7];
 
-    int i = 0; 
-    char c = 'G';
-    flag = 0;
-    if(c[0] == 'G')
-    { 
-      //data = serialGetchar (serial_port);		// receive character serially
-        for(i=0; i<6 ;i++)
-        {
-          if(c[i]==arr[i])
-            {
-              flag++;
-            }
-        }
-    }
-    printf("Value of array: %d\n", flag);
+    // conversion
+    float latitude_ = atof(latitude);
+    float longitude_ = atof(longitude);
+    int satellites_ = atoi(satellites);
+
+    printf("NMEA: %f %s %f %s %d",latitude_,Pole_NS,longitude_,Pole_WE,satellites_);
     
-      usleep(1000000);
-    }
+    return 0;
+
 }
 
