69d7f915033089083cc9d3b293289fa8b6455d21
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-10-28 09:34:35 +0200

    Update BAR.cpp

--- BAR/BAR.cpp
+++ BAR/BAR.cpp
@@ -32,16 +32,17 @@ BAR::~BAR()
 void BAR::Calibrate_BAR() 
 {
     initial_pressure_ = (PRES_*mbar_to_Pa); // Convert mbar to Pascal
-    //printf("Initial pressure: %f\n", initial_pressure_);
     initial_AMSL_ = (T_s/T_G)*(1-pow((initial_pressure_/p_0),T_G*(R/g)))+deviation_; //using international barometric formula to get height
+    printf("Initial pressure: %f\n", initial_AMSL_);
 }
 
 // Converts the bar data into height
 void BAR::ConvertBARData() 
 {
     pressure_ = (PRES_*mbar_to_Pa)+deviation_; // Convert mbar to Pascal.
-    //printf("Pressure: %f\n", pressure_);
+    
     height_AMSL_ = (T_s/T_G)*(1-pow((pressure_/p_0),T_G*(R/g)))+deviation_; // Using international barometric formula to get height
+    printf("Height AMSL: %f\n", height_AMSL_);
     height_AGL_ = height_AMSL_ - initial_AMSL_; // Subtract difference in height.
 }
 
