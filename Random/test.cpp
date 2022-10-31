9daf379aada7d16c2e8b540d5d2e7f3cfd96eb26
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-31 13:47:59 +0100

    Update test.cpp

--- Random/test.cpp
+++ Random/test.cpp
@@ -6,11 +6,20 @@
 #include <unistd.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
+#include <cstddef>
 
 
 int main ()
 {
 char dat;
+
+const std::byte DataStream[] = 
+{
+  0xB5, 0x62, 0x06, 0x01, 0x08, 0x00, 0xF0, 0x01, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2A,
+};
+const int DataStreamLength = sizeof DataStream / sizeof DataStream[0];
+
 int serial_port;
 
     /*freopen ("RDSLog.txt", "w",stdout);
@@ -28,7 +37,7 @@ if (wiringPiSetup () == -1)							/* initializes wiringPi setup */
     return 1 ;
   }
  
-  serialPuts(serial_port,"B56206010800F001000000000000002A");
+  serial_port.write(DataStream, DataStreamLength);
 
   while(1)
   {
