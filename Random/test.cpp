dd84262495be5f31d945b34bb046753052e25ac9
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-10-31 13:08:43 +0100

    Update test.cpp

--- Random/test.cpp
+++ Random/test.cpp
@@ -4,12 +4,42 @@
 #include <assert.h>
 #include <iostream>
 #include <unistd.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
 
 int main ()
 {
-    freopen ("RDSLog.txt", "w",stdout);
+char dat;
+int serial_port;
+    /*freopen ("RDSLog.txt", "w",stdout);
     std::cout << "Hello mister log file" << std::endl;
+    return 0;*/
+if ((serial_port = serialOpen ("/dev/ttyS0", 9600)) < 0)		/* open serial port */
+  {
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+if (wiringPiSetup () == -1)							/* initializes wiringPi setup */
+  {
+    fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+//serialPuts(serial_port,);
+
+  while(1)
+  {
+	  
+		if(serialDataAvail(serial_port))		/* check for any data available on serial port */
+		  { 
+			dat = serialGetchar(serial_port); /* receive character serially */
+            printf("%c",dat);
+            fflush(stdout);
+          }
+
+  }
     return 0;
 }
 
