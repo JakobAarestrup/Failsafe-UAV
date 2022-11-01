8a4f18405c6c3c7a37e3ac655ff31ec72d35f342
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-01 14:05:24 +0100

    Update Neo7.cpp

--- GPS/NEO/Neo7.cpp
+++ GPS/NEO/Neo7.cpp
@@ -108,12 +108,12 @@ void GPS::readGPS(int fd, char sensor_Data, char* d1 , char* d2) // reads GPS se
                 {
                 NMEA[count] = b1; 
                 }
-                printf("%c",NMEA[count]);
+                printf("NMEA: %c",NMEA[count]);
                 count++; // increment
                 b1 = strtok(NULL, ",");  
 
             }
-            printf("NMEA: %s\n",NMEA);
+            //printf("NMEA: %s\n",NMEA);
             NS_ = NMEA[3]; 
             EW_ = NMEA[5]; 
 
