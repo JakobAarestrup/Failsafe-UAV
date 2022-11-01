8128e2bbc288c7b7226eddef8bc909d10f6d3dbc
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-01 19:23:32 +0100

    Updating

--- Random/Tester_2.cpp
+++ Random/Tester_2.cpp
@@ -11,12 +11,56 @@ int main ()
 {
     char def_1[] = "0";
     char def_2[] = "N/A";
-    char* data = def_2;
+    //char* data = def_2;
+    char* d1 = def_1;
+    char* d2 = def_2;
 
-    char* arr[10];
-    arr[0] = data;
-    printf("data = %s\n",data);
-    printf("Arr = %s\n",arr[0]);
+    //char* arr[10];
+    //arr[0] = data;
+    int count = 0;
+    char buff[] = "GPGGA,,5610.29209,N,01011.51302,E,1,09,0.89,74.9,M,42.8,M,,*64";
+    int is_GGA_received_completely= 1;
+    int i = 0;
+    char* NMEA[15];
+
+
+    if(is_GGA_received_completely==1)
+        {
+            printf("\nGGA: %s\n",buff); // kan udkommenteres
+            char arr[15];
+        
+            char* b1 = strchr(buff,","); // first token
+            for(i = 0 ; i < 15 ; i++)
+            {
+                NMEA[i] = b1;
+                b1 = strchr(NULL, ",");
+                printf("b1 = %s\n",b1); 
+
+            }
+            
+            char* NS_ = NMEA[3]; 
+            char* EW_ = NMEA[5]; 
+
+            printf("NS: %s, EW_%s\n",NS_,EW_); 
+            i = 0;
+            printf("NMEA:");
+            for(i = 0 ; i < 15 ; i++)
+            {
+            
+            printf(" %s",NMEA[i]);
+            }
+
+            //conversion
+            /* latitude_   = atof(NMEA[2]);
+            longitude_  = atof(NMEA[4]);
+            SV_ = atoi(NMEA[7]);
+            printf("Lat: %f Long: %f SV: %d\n",latitude_,longitude_,SV_);
+        
+            i = 0; // reset i variable
+            count = 0; // reset count variable
+            is_GGA_received_completely = 0; // reset GGA receive flag variable */
+            usleep(1000000);
+        } 
     return 0;
 }
 
---
+++ Random/Weirdchamp.cpp
@@ -0,0 +1,44 @@
+ #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <iostream>
+#include <unistd.h>
+#include <cstddef>
+
+int main()
+{   
+    char NS_[1];
+    char EW_[1];
+    char gps_string[] = "GPGGA,,1200.201021,,01011.51302,E,1,09,0.89,74.9,M,42.8,M,,*64";
+    char *gps = gps_string;
+    char *start_ptr, *end_ptr;
+
+    start_ptr = strchr(gps, ',');   // find start of field #1
+    start_ptr = strchr(++start_ptr, ','); // find start of field #2 - latitude
+    end_ptr = strchr(++start_ptr, ',');  // find end of field...                   // and zero terminate
+    double latitude = atof(start_ptr);           // Convert char to float & store in variable
+    
+    start_ptr = strchr(++start_ptr, ',');  // find start of field #3 - NS_
+    end_ptr = strchr(++start_ptr, ',');
+    *end_ptr = 0; 
+    strcpy(NS_, start_ptr);
+    //printf("%s\n",NS_);
+    
+    start_ptr = strchr(++end_ptr, ',');  // find start of field #4
+    end_ptr = strchr(++start_ptr, ',');  // find end of field...
+    float longitude = atof(start_ptr); // Convert char to float & store in variable);
+
+    start_ptr = strchr(++start_ptr, ',');  // find start of field #5 
+    end_ptr = strchr(++start_ptr, ',');  // find end of field...
+    *end_ptr = 0;
+                           // and zero terminate
+    printf("latitude: %f %s longitude: %f %s", latitude, NS_ , longitude, start_ptr);
+
+    /* start_ptr = strchr(++start_ptr, ',');  // find start of field #7 - satellites
+    end_ptr = strchr(++start_ptr, ',');  // find end of field...
+    *end_ptr = 0;                            // and zero terminate
+    int SV_ = atoi(start_ptr);        //  Convert string to float & store in variable */
+    printf("latitude: %f ,longitude: %f Sattelites: %d", latitude, longitude,SV_);      //  Convert string to float & store in variable
+ return 0;
+} 
