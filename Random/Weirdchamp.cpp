abae0906887fcaef1ca9d3c2e195549fc2d1de5e
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-01 19:27:35 +0100

    udd

--- Random/Weirdchamp.cpp
+++ Random/Weirdchamp.cpp
@@ -10,12 +10,12 @@ int main()
 {   
     char NS_[1];
     char EW_[1];
-    char gps_string[] = "GPGGA,,1200.201021,,01011.51302,E,1,09,0.89,74.9,M,42.8,M,,*64";
+    char gps_string[] = "GPGGA,,1200.201021,N,01011.51302,E,1,09,0.89,74.9,M,42.8,M,,*64";
     char *gps = gps_string;
-    char *start_ptr, *end_ptr;
+    char *start_ptr, *end_ptr, *start_ptr_origin;
 
-    start_ptr = strchr(gps, ',');   // find start of field #1
-    start_ptr = strchr(++start_ptr, ','); // find start of field #2 - latitude
+    start_ptr_origin = strchr(gps, ',');   // find start of field #1
+    start_ptr = strchr(++start_ptr_origin, ','); // find start of field #2 - latitude
     end_ptr = strchr(++start_ptr, ',');  // find end of field...                   // and zero terminate
     double latitude = atof(start_ptr);           // Convert char to float & store in variable
     
@@ -39,6 +39,6 @@ int main()
     end_ptr = strchr(++start_ptr, ',');  // find end of field...
     *end_ptr = 0;                            // and zero terminate
     int SV_ = atoi(start_ptr);        //  Convert string to float & store in variable */
-    printf("latitude: %f ,longitude: %f Sattelites: %d", latitude, longitude,SV_);      //  Convert string to float & store in variable
+    //printf("latitude: %f ,longitude: %f Sattelites: %d", latitude, longitude,SV_);      //  Convert string to float & store in variable
  return 0;
 } 
