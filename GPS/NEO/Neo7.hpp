01f7c30e565fda5cf29ac09cd507e9dccb6f932d
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-01 10:26:44 +0100

    Update Neo7.hpp

--- GPS/NEO/Neo7.hpp
+++ GPS/NEO/Neo7.hpp
@@ -16,7 +16,7 @@ class GPS
         ~GPS(); // destructor
 
         void openUART(int fd); // open UART serial port
-        void config(int fd, char* message, size_t length);              // configuration of the GPS
+        void config(int fd, const char* message, size_t length);              // configuration of the GPS
         void convertData(float lon_Data, float lat_Data);               // converts GPS data
         void readGPS(int fd, char sensor_Data, char* d1 , char* d2); // reads GPS serial data
         void startLogging(); // sends private data to a log file 
