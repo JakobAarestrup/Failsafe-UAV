71e7d17514ba19c149286f950360685f23f7a52a
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-02 21:25:07 +0100

    Update

--- GPS/NEO/Neo7.cpp
+++ GPS/NEO/Neo7.cpp
@@ -189,18 +189,20 @@ double GPS::getLatitude() const // returns latitude
     return latitude_;
 }
 
-char GPS::getEastWest(char EW[]) // returns either a East pole or West pole
+void GPS::getEastWest(char EW[]) // returns either a East pole or West pole
 {
-    strncpy(EW,EW_);
-    printf("EW_: %s\n", EW_);
-    printf("EW:%s\n", EW);
-    return EW;
+    int strLength = strlen(EW_); //finds length of the array
+    for (int i = 0; i < strLength; i++) {
+        EW[i] = EW_[strLength-1-i]; //copies UserInput in reverse to TempInput
+    }
+    EW[strLength] = '\0'; //adds NULL character at end
 }
 
-char GPS::getNorthSouth(char NS[]) // returns either a East pole or West pole
+void GPS::getNorthSouth(char NS[]) // returns either a East pole or West pole
 {
-    strncpy(NS,NS_);
-    printf("NS_: %s\n", NS_);
-    printf("NS: %s\n", NS); 
-    return NS;
+    int strLength = strlen(NS_); //finds length of the array
+    for (int i = 0; i < strLength; i++) {
+        NS[i] = NS_[strLength-1-i]; //copies UserInput in reverse to TempInput
+    }
+    NS[strLength] = '\0'; //adds NULL character at end
 }
--- Random/Tester_2.cpp
+++ Random/Tester_2.cpp
@@ -7,53 +7,20 @@
 #include <cstddef>
 #include <windows.h>
 
-int main()
+void getNorthSouth(char NS[])
 {   
-    
-    char NS_[1];
-    char NS_s[1];
-    char EW_[1];
-    char gps_string[100] = "GPGGA,,1200.201021,N,01011.51302,E,1,09,0.89,74.9,M,42.8,M,,*64";
-    char gps_string2[100] = "GPGGA,,,,,,,,,,,,,,,";
-   
-    char *gps = gps_string;
-    char *gps_2 = gps_string2;
+    char NS_[10] ="N";
+    int strLength = strlen(NS_); //finds length of the array
+    for (int i = 0; i < strLength; i++) {
+        NS[i] = NS_[strLength-1-i]; //copies UserInput in reverse to TempInput
+    }
+    NS[strLength] = '\0'; //adds NULL character at end
+}
+int main()
+{
+char NS[1];
+getNorthSouth(NS);
+printf("%s",NS);
 
-    char *start_ptr;
-    char* end_ptr; 
-    char* start_ptr_origin ;
-    double latitude , longitude;
-    int SV_;
-    int i = 5;
-
-    start_ptr_origin = strchr(gps, ',');   // find start of field #1
-    start_ptr = strchr(++start_ptr_origin, ','); // find start of field #2 - latitude
-    end_ptr = strchr(++start_ptr, ',');  // find end of field...  
-    latitude = atof(start_ptr);   // Convert char to float & store in variable
-    
-    start_ptr = strchr(++start_ptr, ',');  // find start of field #3 - NS_
-    end_ptr = strchr(++start_ptr, ','); // find end of field... 
-    *end_ptr = '\0';   // and zero terminate
-    strcpy(NS_, start_ptr);
-    
-    start_ptr += 2; // field 4
-    end_ptr = strchr(++start_ptr, ','); // find end of field...
-    *end_ptr = '\0';  // and zero terminate
-    longitude = atof(start_ptr); 
-
-    start_ptr = strchr(++end_ptr, ',');
-    --start_ptr; // find start of field #5 - EW
-    end_ptr = strchr(start_ptr, ','); // find end of field... 
-    *end_ptr = '\0';  // and zero terminate
-
-    strcpy(EW_, start_ptr);
-
-    start_ptr = strchr(++end_ptr, ','); // find start of field 7
-    end_ptr = strchr(++start_ptr, ','); // find end of field... 
-    *end_ptr = '\0';  // and zero terminate
-    SV_ = atoi(start_ptr); // Convert char to int & store in variable
-    
-    printf("latitude: %f %s longitude: %f %s Sattelites: %d\n", latitude, NS_, longitude, EW_, SV_);      //  Convert string to float & store in variable
-
- return 0;
-} 
+return 0;
+}
