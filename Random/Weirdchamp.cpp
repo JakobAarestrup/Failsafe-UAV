49cbb00269aa6465a2edf91da0e3a77fbf2e9b6f
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-02 19:34:52 +0100

    Update Weirdchamp.cpp

--- Random/Weirdchamp.cpp
+++ Random/Weirdchamp.cpp
@@ -23,6 +23,7 @@ int main()
     {
     /* char gps_string[100] = "GPGGA,,1200.201021,N,01011.51302,E,1,09,0.89,74.9,M,42.8,M,,*64";
     char *gps = gps_string; */
+    183251.20,,,,,0,00,99.99,,,,,,*68*68;
     char gps_string2[100] = "$GPGGA,,,,,,,,,,,,,,,";
     char *gps_2 = gps_string2; 
     start_ptr_origin = strchr(gps_2, ',');   // find start of field #1
