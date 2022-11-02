75dfb764c7a3f6171b6dbc26a30ca36bcd889640
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-02 19:18:26 +0100

    Update Neo7.hpp

--- GPS/NEO/Neo7.hpp
+++ GPS/NEO/Neo7.hpp
@@ -17,15 +17,15 @@ class GPS
 
         int openUART(int fd); // open UART serial port
         void config(int fd, const char* message, size_t length);              // configuration of the GPS
-        void convertData(double lon_Data, double lat_Data, char* NS, char* EW);   // converts GPS data
+        void convertData(double lon_Data, double +lat_Data, char* NS, char* EW);   // converts GPS data
         void readGPS(int fd, char sensor_Data, char* d1 , char* d2); // reads GPS serial data
         void startLogging(); // sends private data to a log file 
 
         int   getSV() const;         // returns amount of satellites
         double getLongitude() const;  // returns longitude
         double getLatitude()  const;  // returns latitude
-        char* getNorthSouth() const; // returns either a north pole or south pole
-        char* getEastWest() const;   // returns either a East pole or West pole
+        char getNorthSouth() const; // returns either a north pole or south pole
+        char getEastWest() const;   // returns either a East pole or West pole
 
     private: // Coordinates from the GPS
 
