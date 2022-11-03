b4331a110a12e36ca5c795655e7e4a005877fac0
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-03 18:38:14 +0100

    Update IMU.hpp

--- IMU/IMU.hpp
+++ IMU/IMU.hpp
@@ -6,8 +6,9 @@ public:
     void ConvertGyroData(int gY, int gX, int gZ);
     void ConvertMagData(int mY, int mX, int mZ);
     //float getACCData(); // Returns ACC data
+    float getMagData();
     float getGyroData(); // Returns Gyro data
-	float gyr_Zangle_ = 0.0; // Gyroscope Z-angle
+/* 	float gyr_Zangle_ = 0.0; // Gyroscope Z-angle
     float rate_gyr_y_ = 0.0;   // Gyroscope X-angle in deg/s
 	float rate_gyr_x_ = 0.0;   // Gyroscope Y-angle in deg/s
 	float rate_gyr_z_ = 0.0;   // Gyroscope Z-angle in deg/s
@@ -18,8 +19,7 @@ public:
     int gyro_Z_; // Gyroscope Z-axis
     int mag_X_; // Magnetometer X-axis
     int mag_Y_; // Magnetometer Y-axis
-    int mag_Z_; // Magnetometer Z-axis
-    */
+    int mag_Z_; // Magnetometer Z-axis */
     float acc_X_; // Accelerometer X-axis
     float acc_Y_; // Accelerometer Y-axis
     float gyroData; // Gyroscope data
