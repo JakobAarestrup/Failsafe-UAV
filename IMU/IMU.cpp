f3d721bf6bdbf99a4fd5c2ca951d45b25d2dfbd0
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-03 16:09:38 +0100

    Update

--- IMU/IMU.cpp
+++ IMU/IMU.cpp
@@ -31,7 +31,7 @@ float aXg = (aX*XL_Sensitivity)/1000; // value in g
 float aYg = (aY*XL_Sensitivity)/1000; // value in g
 float aZg = (aZ*XL_Sensitivity)/1000; // value in g
 
-float A = pow(aXg,2)+ pow(aYg,2) + pow(aZg,2); // force on object
+float A = sqrt(pow(aXg,2)+ pow(aYg,2) + pow(aZg,2)); // force on object
 float XL_xdeg = acos(aXg/A);//*(pi/180);
 float XL_ydeg = acos(aYg/A);//*(pi/180);
 float XL_zdeg = acos(aZg/A);//*(pi/180);
