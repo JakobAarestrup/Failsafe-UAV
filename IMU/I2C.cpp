af7755c7a9587a649ca0c30f3211e28d43095123
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-04 17:25:13 +0100

    Update I2C.cpp

--- IMU/I2C.cpp
+++ IMU/I2C.cpp
@@ -73,7 +73,7 @@ int I2C::ReadI2C_16bit(int ADDR, int reg)
 
         return I2C_Data_; // Return i2c data
     }   */
-
+    int I2C_Data_ = (Reg_data1 | Reg_data2 << 8);
     return I2C_Data_; // Return i2c data
 }
 
