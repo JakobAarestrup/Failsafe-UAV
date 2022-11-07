9b5ed8c587723679cbbf6fc41a07a5beeb47aa48
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-07 11:12:42 +0100

    update

--- IMU/I2C.cpp
+++ IMU/I2C.cpp
@@ -60,7 +60,7 @@ int I2C::ReadI2C_16bit(int ADDR, int reg)
         exit(1);
     }
     else
-    {  
+/*     {  
         int I2C_Data_ = (Reg_data1 | Reg_data2 << 8); // Bitshift registers together
         if(I2C_Data_ < 0x8000) 
         {
@@ -72,7 +72,8 @@ int I2C::ReadI2C_16bit(int ADDR, int reg)
         }
 
         return I2C_Data_; // Return i2c data
-    }   
+    }  */ 
+    int I2C_Data_ = (Reg_data1 | Reg_data2 << 8); 
     return I2C_Data_; // Return i2c data
 }
 
--- IMU/IMU.cpp
+++ IMU/IMU.cpp
@@ -24,13 +24,13 @@ IMU::~IMU()
 void IMU::ConvertACCData(int aY, int aX, int aZ)
 {
 
-float XL_Sensitivity = 0.061; // +/-2g
+float XL_Sensitivity = 0.01; // +/-2g
 double pi = 3.14159265358979;
 double conv_Rate = 0.005493247882811;
 printf("non-Converted - X: %d, Y: %d Z: %d\n",aX,aY,aZ);
-float aXg = (aX*XL_Sensitivity)/1000; // value in g
-float aYg = (aY*XL_Sensitivity)/1000; // value in g
-float aZg = (aZ*XL_Sensitivity)/1000; // value in g
+float aXg = (aX*XL_Sensitivity); // value in g
+float aYg = (aY*XL_Sensitivity); // value in g
+float aZg = (aZ*XL_Sensitivity); // value in g
 
 double aXraw = aX*conv_Rate; 
 double aYraw = aY*conv_Rate;
@@ -43,8 +43,8 @@ double aYraw_off = (aY+0xFFFF)*conv_Rate;
 printf("Raw - X: %lf, Y: %lf \n\n",aXraw, aYraw);
 printf("Raw_off - X: %lf, Y: %lf \n\n",aXraw_off, aYraw_off);
 float A = sqrt(pow(aXg,2)+ pow(aYg,2) + pow(aZg,2)); // force on object
-double XL_xdeg = (atan2(-aXg,-aZg))/(pi/180)+180;
-double XL_ydeg = (atan2(-aYg,-aZg)/(pi/180))+180;
+double XL_xdeg = (atan2(-aXg,-aZg))/(pi/180);//+180;
+double XL_ydeg = (atan2(-aYg,-aZg)/(pi/180));//+180;
 double XL_zdeg = 180-(atan2(-aYg,-aZg)/(pi/180));
 
 printf("Converted - X: %f, Y: %f Z: %f\n",XL_xdeg,XL_ydeg, XL_zdeg);
