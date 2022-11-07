630fc3f0cf214d8b23f0b9b1626ab7a1bd0d66d8
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-07 15:35:45 +0100

    Update IMU.cpp

--- IMU/IMU.cpp
+++ IMU/IMU.cpp
@@ -6,63 +6,60 @@
 // Definitions
 #define PI 3.14159265358979323846
 #define RAD_TO_DEG 57.29578
-#define DT 0.02         // [s/loop] loop period. 20ms
-#define AA 0.97         // complementary filter constant
-#define A_GAIN 0.0573    // [deg/LSB]
-#define G_GAIN 0.070     // [deg/s/LSB]
+#define DT 0.02       // [s/loop] loop period. 20ms
+#define AA 0.97       // complementary filter constant
+#define A_GAIN 0.0573 // [deg/LSB]
+#define G_GAIN 0.070  // [deg/s/LSB]
 
 // Constructor
 IMU::IMU()
-{}
+{
+}
 
 // Destructor
 IMU::~IMU()
 {
-    //delete [] sensor_handle_,gyr_Xangle_,gyr_Yangle_, gyr_Zangle_,roll_,pitch_; // Delete private variables
-    //delete [] rate_gyr_x_,rate_gyr_y_,rate_gyr_z_,mag_X_,mag_Y_,mag_Z_; // Delete private variables
+    // delete [] sensor_handle_,gyr_Xangle_,gyr_Yangle_, gyr_Zangle_,roll_,pitch_; // Delete private variables
+    // delete [] rate_gyr_x_,rate_gyr_y_,rate_gyr_z_,mag_X_,mag_Y_,mag_Z_; // Delete private variables
 }
 void IMU::ConvertACCData(int aX, int aY, int aZ)
 {
 
-printf("non-Converted - X: %d, Y: %d Z: %d\n",aX,aY,aZ);
+    printf("non-Converted - X: %d, Y: %d Z: %d\n", aX, aY, aZ);
 
-double XL_xdeg = (atan2(aY,aZ)+PI)*RAD_TO_DEG;
-double XL_ydeg = (atan2(aZ,aY)+PI)*RAD_TO_DEG;
-double XL_zdeg = 180-(atan2(-aY,-aZ)/(PI/180));
-XL_xdeg -= (double)180.0;
-		if ( XL_ydeg > 90)
-				XL_ydeg -= (double)270;
-		else
-			XL_ydeg += (double)90; 
-/* if (XL_xdeg >180)
-	XL_xdeg -= (double)360.0;
+    double XL_xdeg = (atan2(-aX, -aZ)) / (PI / 180)+180;
+    double XL_ydeg = (atan2(-aZ, -aY)) / (PI / 180)+180;
+    double XL_zdeg = 180 - (atan2(-aY, -aZ) / (PI / 180));
+    XL_xdeg -= (double)180.0;
+    if (XL_ydeg > 90)
+        XL_ydeg -= (double)270;
+    else
+        XL_ydeg += (double)90;
+    /* if (XL_xdeg >180)
+        XL_xdeg -= (double)360.0;
 
-XL_ydeg-=90;
+    XL_ydeg-=90;
 
-if (XL_ydeg > 180)
-        XL_ydeg -= (double)360.0; */
+    if (XL_ydeg > 180)
+            XL_ydeg -= (double)360.0; */
 
-
-printf("Converted - X: %f, Y: %f Z: %f\n",XL_xdeg,XL_ydeg, XL_zdeg);
-
+    printf("Converted - X: %f, Y: %f Z: %f\n", XL_xdeg, XL_ydeg, XL_zdeg);
 }
 
 void IMU::ConvertGyroData(int gX, int gY, int gZ)
 {
-        rate_gyr_x_ = (float) gX  * G_GAIN;
-		rate_gyr_y_ = (float) gY  * G_GAIN;
-		rate_gyr_z_ = (float) gZ  * G_GAIN;  
-        printf("GyroX: %f, GyroY: %f, GyroZ: %f\n", rate_gyr_x_, rate_gyr_y_,rate_gyr_z_); 
-        gyroXangle_+=rate_gyr_x_*DT;
-		gyroYangle_+=rate_gyr_y_*DT;
-		gyroZangle_+=rate_gyr_z_*DT;
-        printf("GyroXangle: %f, GyroYangle: %f, GyroZangle: %f\n", gyroXangle_, gyroYangle_,gyroZangle_); 
-
+    rate_gyr_x_ = (float)gX * G_GAIN;
+    rate_gyr_y_ = (float)gY * G_GAIN;
+    rate_gyr_z_ = (float)gZ * G_GAIN;
+    printf("GyroX: %f, GyroY: %f, GyroZ: %f\n", rate_gyr_x_, rate_gyr_y_, rate_gyr_z_);
+    gyroXangle_ += rate_gyr_x_ * DT;
+    gyroYangle_ += rate_gyr_y_ * DT;
+    gyroZangle_ += rate_gyr_z_ * DT;
+    printf("GyroXangle: %f, GyroYangle: %f, GyroZangle: %f\n", gyroXangle_, gyroYangle_, gyroZangle_);
 }
 
 void IMU::ConvertMagData(int mY, int mX, int mZ)
 {
-    
 }
 
 /* float IMU::getACCData() // Returns ACC data
