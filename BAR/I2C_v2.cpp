84c2859d56fba9f1406fc4747bd9e313d9db4d17
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-11 13:36:51 +0100

    i2c test

--- BAR/I2C_v2.cpp
+++ BAR/I2C_v2.cpp
@@ -6,7 +6,6 @@
 #include <fcntl.h>
 #include "LIS3MDL.hpp"
 #include "LSM6DSOX.hpp"
-#include "BAR.hpp"
 extern "C"
 {
     #include <linux/i2c-dev.h>
@@ -25,6 +24,7 @@ I2C::~I2C()
 // Open i2c communication
 void I2C::InitializeI2C()
 {
+    /*
     //First IMU:
     I2Cdev::writeBytes(LSM6DSOX_ADDR1, LSM6DSOX_INT1_CTRL, 1, 0b00000011); // Enable gyroscope and accelerometer data interrupt
     I2Cdev::writeBytes(LSM6DSOX_ADDR1, LSM6DSOX_CTRL2_G, 1, 0b10011100); // Gyro = 416 Hz (High-Performance mode)
@@ -48,6 +48,7 @@ void I2C::InitializeI2C()
     I2Cdev::writeBytes(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG2, 1, 0b00000000);// FS = 00 (+/- 4 gauss full scale)
     I2Cdev::writeBytes(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG3, 1, 0b00000000);// MD = 00 (continuous-conversion mode)
     I2Cdev::writeBytes(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG4, 1, 0b00001100);// OMZ = 11 (ultra-high-performance mode for Z)
+    */
 }
 
 // Write to I2C device register
@@ -57,15 +58,21 @@ void I2C::WriteI2C(int ADDR, int reg, int length, int data)
 }
 
 // Read from i2c device register
-float I2C::ReadI2C(int ADDR, int reg, int length)
+float I2C::ReadI2C(int ADDR, int reg, int length, int HandleI2C)
 {
-    if(length==3) // 24-bit read likely from the Barometer
+    if(HandleI2C_ == 3) // 24-bit read likely from the Barometer
     {
         uint8_t buffer[3];
         I2Cdev::readBytes(ADDR, reg, length, buffer);
         I2CData_ = (buffer[0] << 16) | (buffer[1] << 8) | buffer[2];
     }
-    if else (length==2) // 16-bit read likely from the IMU
+    if else  (HandleI2C == 2) // 16-bit read likely from the IMU
+    {
+        uint8_t buff[2];
+        I2Cdev::readBytes(ADDR, reg, length, buff);
+        I2CData_ = buff[0]<<8 | buff[1];
+    }
+    if else(HandleI2C == 1)
     {
         I2Cdev::readBytes(ADDR, reg, length, Buffer1_);
         I2Cdev::readBytes(ADDR, reg+1, length, Buffer2_);
