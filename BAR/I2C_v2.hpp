428ac5f2df20a9c8104c6ad09b20f5e9c3160954
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-11 13:59:31 +0100

    Update I2C_v2.hpp

--- BAR/I2C_v2.hpp
+++ BAR/I2C_v2.hpp
@@ -4,11 +4,10 @@ public:
     I2C(); // Constructor
     ~I2C(); // Destructor
     void InitializeI2C();
-    void WriteI2C(int ADDR, int reg, int length, uint8_t data); // Write to I2C device register
+    void WriteI2C(int ADDR, int reg, int length, unsigned char data); // Write to I2C device register
     float ReadI2C(int ADDR, int reg, int length, int HandleI2C); // Read from I2C device register
 private:
-    typedef uint8_t;
     float I2CData_; // I2C device data
-    uint8_t Buffer1_;
-    uint8_t Buffer2_;
+    unsigned char Buffer1_;
+    unsigned char Buffer2_;
 };
