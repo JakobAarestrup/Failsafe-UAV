5e51a1efc9af7fade05d247f4acca34384d20981
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-11 13:21:33 +0100

    added handle to bar calibration

--- IMU/I2C_v2.hpp
+++ IMU/I2C_v2.hpp
@@ -5,7 +5,7 @@ public:
     ~I2C(); // Destructor
     void InitializeI2C();
     void WriteI2C(int ADDR, int reg, int length, int data); // Write to I2C device register
-    int ReadI2C(int ADDR, int reg, int length, float recipient); // Read from I2C device register
+    int ReadI2C(int ADDR, int reg, int length, int HandleI2C); // Read from I2C device register
 private:
     float I2CData_; // I2C device data
     float Buffer1_;
