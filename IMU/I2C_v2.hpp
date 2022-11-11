35cec82b20fa9966850df6e2ce3d9e34b71f4987
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-11 14:22:46 +0100

    e

--- IMU/I2C_v2.hpp
+++ IMU/I2C_v2.hpp
@@ -4,10 +4,10 @@ public:
     I2C(); // Constructor
     ~I2C(); // Destructor
     void InitializeI2C();
-    void WriteI2C(int ADDR, int reg, int length, int data); // Write to I2C device register
+    void WriteI2C(int ADDR, int reg, int length, unsigned char* data); // Write to I2C device register
     float ReadI2C(int ADDR, int reg, int length, int HandleI2C); // Read from I2C device register
 private:
     float I2CData_; // I2C device data
-    float Buffer1_;
-    float Buffer2_;
+    unsigned char* Buffer1_;
+    unsigned char* Buffer2_;
 };
