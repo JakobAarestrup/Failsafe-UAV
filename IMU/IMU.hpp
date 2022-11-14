e7c406ed346fb4b87064aba1c851a57f051f1a03
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-14 13:52:38 +0100

    Update IMU.hpp

--- IMU/IMU.hpp
+++ IMU/IMU.hpp
@@ -6,9 +6,8 @@ public:
     void ConvertACCData(float aX, float aY, float aZ);
     void ConvertGyroData(float gX, float gY, float gZ);
     void ConvertMagData(float mX, float mY);
-
+    void calibrateIMU();
 private:
-    double magYaw_ = 0.0;
     double rate_gyr_y_ = 0.0;   // Gyroscope X-angle in deg/s
 	double rate_gyr_x_ = 0.0;   // Gyroscope Y-angle in deg/s
 	double rate_gyr_z_ = 0.0;   // Gyroscope Z-angle in deg/s
@@ -17,4 +16,5 @@ private:
 	double gyroZangle_ = 0.0;
     double XL_Roll_ = 0.0; // Accelerometer X-axis
     double XL_Pitch_ = 0.0; // Accelerometer Y-axis
+    double magYaw_ = 0.0;
 };
