ac0d972cec0db940dd76988c6f5825518d3f6032
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-14 12:52:01 +0100

    Update I2C_v2.cpp

--- IMU/I2C_v2.cpp
+++ IMU/I2C_v2.cpp
@@ -96,7 +96,8 @@ float I2C::readI2C(int ADDR, int reg, int length, int HandleI2C)
         } */
 
            printf("Buff: %d,buff2:%d\n", buff, nbuff);
-        int I2CData_ = (buff | nbuff << 8); 
+        int I2CData_ = (buff | nbuff << 8);
+        printf("Samlet: %d\n", I2C_Data_);
 
     }
     else // 8-bit read
