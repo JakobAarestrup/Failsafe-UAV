a4aba548bfe5f534e930fd9e474adb90949c85c8
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-17 00:17:21 +0100

    Update

--- IMU/Gyro_calibration.cpp
+++ IMU/Gyro_calibration.cpp
@@ -15,10 +15,13 @@ int main()
 
 I2C I1;
 int N = 0;
-int gyro_sensitivity = 
+int gyro_sensitivity = 17.5;
 IMU IMU1;
 
-// Enable magnetometer from IMU1
+// Enable  from IMU1
+I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
+I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b01100100);   // Gyro = 416 Hz (High-Performance mode) 500 dps
+I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
 
 // Main loop
 while(1)
@@ -28,11 +31,15 @@ while(1)
     float gy = I1.ReadI2C_16bit(LSM6DSOX_ADDR2, LSM6DSOX_OUT_Y_L_G);
     float gz = I1.ReadI2C_16bit(LSM6DSOX_ADDR2, LSM6DSOX_OUT_Z_L_G);
 
+    float gxc = (gx*gyro_sensitivity)/1000; 
+    float gyc = (gy*gyro_sensitivity)/1000; 
+    float gzc = (gz*gyro_sensitivity)/1000;
+
     
 
-    if (N < 2000)
+    if (N < 500)
     {
-        printf("%f %f %f \n",mxc,myc,mzc);
+        printf("%f %f %f \n",gxc,gyc,gzc);
         usleep(20000); // 20ms Delay
         N = N+1;
     }
--- Random/test.cpp
+++ Random/test.cpp
@@ -5,13 +5,13 @@
 #include <cstddef>
 #include <unistd.h>
 #include <iostream>
-int main()
+/* int main()
 {
 float mx = 28.061972;
 float my = 9.909383;
 float mz = -52.645428;
 
-//float magData[3] = {mx,my,mz};
+float magData[3] = {mx,my,mz};
 
 float A[3][3] = { {1.002979, 0.039343, -0.014713}, 
                   {0.039343, 1.019943, -0.006826}, 
@@ -19,19 +19,54 @@ float A[3][3] = { {1.002979, 0.039343, -0.014713},
 
 float b[3] {7.977849,3.137438,-5.371644};
 
-float magxC = A[0][0]*mx + A[0][1]*my + A[0][2]*mz;
-float bxC   = A[0][0]*b[0] + A[0][1]*b[1] + A[0][2]*b[2];
+// magCalibX = A[0,:]*(magData[:]-b[:])
+float magxC = A[0][0]*mx + A[0][1]*my + A[0][2]*mz;       // A[0,:]*magData[:]
+float bxC   = A[0][0]*b[0] + A[0][1]*b[1] + A[0][2]*b[2]; // A[0,:]*b[:]
 float magCalibX = magxC-bxC;
 
-float magyC = A[1][0]*mx + A[1][1]*my + A[1][2]*mz;
-float byC   = A[1][0]*b[0] + A[1][1]*b[1] + A[1][2]*b[2];
+// magCalibY = A[1,:]*(magData[:]-b[:])
+float magyC = A[1][0]*mx + A[1][1]*my + A[1][2]*mz;       // A[1,:]*magData[:]
+float byC   = A[1][0]*b[0] + A[1][1]*b[1] + A[1][2]*b[2]; // A[1,:]*b[:]
 float magCalibY = magyC-byC;
 
-float magzC = A[2][0]*mx + A[2][1]*my + A[2][2]*mz;
-float bzC   = A[2][0]*b[0] + A[2][1]*b[1] + A[2][2]*b[2];
+// magCalibZ = A[2,:]*(magData[:]-b[:])
+float magzC = A[2][0]*mx + A[2][1]*my + A[2][2]*mz;       // A[2,:]*magData[:]
+float bzC   = A[2][0]*b[0] + A[2][1]*b[1] + A[2][2]*b[2]; // A[2,:]*b[:]
 float magCalibZ = magzC-bzC;
+
 
 
 printf("magCalibX: %f magCalibY: %f  magCalibZ: %f non-calibrated", magCalibX,magCalibY,magCalibZ);
 return 0;
-}
+} */
+
+int main()
+{
+float mx = 28.061972;
+float my = 9.909383;
+float mz = -52.645428;
+
+// hard-iron distortion
+float bx = 7.977849; 
+float by = 3.137438; 
+float bz = -5.371644;
+
+// soft-iron distortion
+float A[3][3] = { {1.002979, 0.039343, -0.014713}, 
+                  {0.039343, 1.019943, -0.006826}, 
+                  {-0.014713, -0.006826, 1.014517} };
+
+// formel magdataCalibrated = A(magdata-b)
+
+float mxb = mx - bx; 
+float myb = my - by;
+float mzb = mz - bz;
+
+float magCalibX = A[0][0]*mxb + A[0][1]*myb + A[0][2]*mzb;  // A[0,:]*(magdata-b)
+float magCalibY = A[1][0]*mxb + A[1][1]*myb + A[1][2]*mzb;  // A[1,:]*(magdata-b)
+float magCalibZ = A[2][0]*mxb + A[2][1]*myb + A[2][2]*mzb;  // A[2,:]*(magdata-b)
+
+
+printf("magCalibX: %f magCalibY: %f  magCalibZ: %f non-calibrated", magCalibX,magCalibY,magCalibZ);
+return 0;
+} 
