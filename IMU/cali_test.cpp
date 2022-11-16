d4dbfc9fc52cad91d76c9b8e6bb9659f00ef7cca
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-16 19:19:39 +0100

    Update cali_test.cpp

--- IMU/cali_test.cpp
+++ IMU/cali_test.cpp
@@ -14,9 +14,9 @@ float magData[3] = {mx,my,mz};
 float A[3][3] = {1.002979, 0.039343, -0.014713, 0.039343, 1.019943, -0.006826, -0.014713, -0.006826, 1.014517};
 float b[3] = {7.977849, 3.137438, -5.371644};
 
-printf("magData: \f",magData);
-printf("A: \f",A);
-printf("b: \f",b);
+printf("magData: \f\n",magData);
+printf("A: \f\n",A);
+printf("b: \f\n",b);
 
 //float magC[3] = A*(magData-b);
 
