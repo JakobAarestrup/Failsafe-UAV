7017035afe89e9c924efa03b2b1b6db9df377c56
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-16 21:58:41 +0100

    Update cali_test.cpp

--- IMU/cali_test.cpp
+++ IMU/cali_test.cpp
@@ -3,7 +3,10 @@
 #include <unistd.h>
 #include <iostream>
 #include "linalg.h"
-using namespace linalg::aliases;
+
+#define float3 linalg::aliases::float3
+#define float3x3 linalg::aliases::float3x3
+//using namespace linalg::aliases;
 
 int main() 
 {
@@ -11,12 +14,8 @@ float mx = 28.061972;
 float my = 9.909383;
 float mz = -52.645428;
 
-//float magData[3] = {mx,my,mz};
-
 float3 magData {mx,my,mz};
 
-//float A[3][3] = {1.002979, 0.039343, -0.014713, 0.039343, 1.019943, -0.006826, -0.014713, -0.006826, 1.014517};
-//float b[3] = {7.977849, 3.137438, -5.371644};
 float3x3 A {{1.002979, 0.039343, -0.014713}, 
             {0.039343, 1.019943, -0.006826}, 
             {-0.014713, -0.006826, 1.014517}};
