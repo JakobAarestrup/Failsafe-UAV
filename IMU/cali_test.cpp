3fde76e6292feadd8fd835b6b3311c2ce9516a64
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-16 19:54:56 +0100

    Update cali_test.cpp

--- IMU/cali_test.cpp
+++ IMU/cali_test.cpp
@@ -20,7 +20,7 @@ float3 magData {28.061972,9.909383,-52.645428};
 float3x3 A {{1.002979, 0.039343, -0.014713}, {0.039343, 1.019943, -0.006826}, {-0.014713, -0.006826, 1.014517}};
 float3 b {7.977849,3.137438,-5.371644};
 
-float3 magC {A mul (magData-b)};
+float3 magC {dot(A,(magData-b))};
 
 printf("Calibrated magData: \f",magC);
 
