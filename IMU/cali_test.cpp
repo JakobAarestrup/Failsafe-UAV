8ff86bd34566b96a7279492532897d88c33dd8f3
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-16 19:46:49 +0100

    linalg added

--- IMU/cali_test.cpp
+++ IMU/cali_test.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
+#include "linalg.h"
+using namespace linalg::aliases;
 
 int main() 
 {
@@ -9,16 +11,16 @@ float mx = 28.061972;
 float my = 9.909383;
 float mz = -52.645428;
 
-float magData[3] = {mx,my,mz};
+//float magData[3] = {mx,my,mz};
 
-float A[3][3] = {1.002979, 0.039343, -0.014713, 0.039343, 1.019943, -0.006826, -0.014713, -0.006826, 1.014517};
-float b[3] = {7.977849, 3.137438, -5.371644};
+float3x1 magData {mx,my,mz};
 
-printf("magData: \f\n",magData);
-printf("A: \f\n",A);
-printf("b: \f\n",b);
+//float A[3][3] = {1.002979, 0.039343, -0.014713, 0.039343, 1.019943, -0.006826, -0.014713, -0.006826, 1.014517};
+//float b[3] = {7.977849, 3.137438, -5.371644};
+float3x3 A {{1.002979, 0.039343, -0.014713}, {0.039343, 1.019943, -0.006826}, {-0.014713, -0.006826, 1.014517}};
+float3x1 b {7.977849,3.137438,-5.371644};
 
-float magC[3] = A*(magData-b);
+float3x1 magC {A*(magData-b)};
 
 printf("Calibrated magData: \f",magC);
 
