3b3188552fb8fea1a3561da558161563dfbf2d10
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-17 14:30:14 +0100

    Update Gyro_calibration.cpp

--- IMU/Gyro_calibration.cpp
+++ IMU/Gyro_calibration.cpp
@@ -29,7 +29,7 @@ int gyro_sensitivity = 17.5;
 
 // Enable  from IMU2
 I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
-I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b01100100);   // Gyro = 416 Hz (High-Performance mode) 500 dps
+I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b01000000);   // Gyro = 105 Hz (normal mode) 250 dps
 I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
 
 // Main loop
@@ -47,7 +47,7 @@ I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
         if (N < 2000)
         {
             printf("%f %f %f \n",gx,gy,gz);
-            usleep(20000); // 20ms Delay
+            //usleep(20000); // 20ms Delay
             N = N+1;
         }
         else
@@ -56,10 +56,10 @@ I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
             break;
         }
 
-       /*  while(mymillis() - startofloop < 50)
+       while(mymillis() - startofloop < 200)
         {
-            usleep(100);
-        } */
+            usleep(20000);
+        }
 
     }
 
