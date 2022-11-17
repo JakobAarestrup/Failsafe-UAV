ef93dce4e0bb803f748c176b020d2ffea2586c21
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-17 15:41:22 +0100

    Update Gyro_calibration.cpp

--- IMU/Gyro_calibration.cpp
+++ IMU/Gyro_calibration.cpp
@@ -26,9 +26,9 @@ int N = 0;
 int gyro_sensitivity = 70;
 
 // Enable  from IMU2
-I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
-I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b01010000);   // Gyro = 208 Hz (normal mode) 250 dps
-I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
+I2Cdev::writeByte(LSM6DSOX_ADDR1, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
+I2Cdev::writeByte(LSM6DSOX_ADDR1, LSM6DSOX_CTRL2_G, 0b01010000);   // Gyro = 208 Hz (normal mode) 250 dps
+I2Cdev::writeByte(LSM6DSOX_ADDR1, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
 int startofloop  = mymillis();
 struct  timeval tvBegin, tvEnd,tvDiff;
 gettimeofday(&tvBegin, NULL);
@@ -39,9 +39,9 @@ gettimeofday(&tvBegin, NULL);
         // Read from Gyro from IMU2
         while(1)
         {
-        float gx = I1.readI2C(LSM6DSOX_ADDR2, LSM6DSOX_OUT_X_L_G,1,1);
-        float gy = I1.readI2C(LSM6DSOX_ADDR2, LSM6DSOX_OUT_Y_L_G,1,1);
-        float gz = I1.readI2C(LSM6DSOX_ADDR2, LSM6DSOX_OUT_Z_L_G,1,1);
+        float gx = I1.readI2C(LSM6DSOX_ADDR1, LSM6DSOX_OUT_X_L_G,1,1);
+        float gy = I1.readI2C(LSM6DSOX_ADDR1, LSM6DSOX_OUT_Y_L_G,1,1);
+        float gz = I1.readI2C(LSM6DSOX_ADDR1, LSM6DSOX_OUT_Z_L_G,1,1);
 
         float gxc = (gx*gyro_sensitivity)/1000; 
         float gyc = (gy*gyro_sensitivity)/1000; 
