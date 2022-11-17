d9ffc507398417ec57c0f8556610e51c7d126d0e
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-17 15:22:27 +0100

    Update Gyro_calibration.cpp

--- IMU/Gyro_calibration.cpp
+++ IMU/Gyro_calibration.cpp
@@ -26,7 +26,7 @@ int N = 0;
 int gyro_sensitivity = 70;
 
 // Enable  from IMU2
-//I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
+I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
 I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b10011100);   // Gyro = 105 Hz (normal mode) 250 dps
 I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
 int startofloop  = mymillis();
