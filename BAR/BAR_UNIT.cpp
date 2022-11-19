d3bf4fc9050bf2f0c6267973bfe406a99e3bf7d0
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-19 11:47:33 +0100

    hpp

--- BAR/BAR_UNIT.cpp
+++ BAR/BAR_UNIT.cpp
@@ -1,21 +1,4 @@
-#include <math.h>
-
-class BAR_UNIT
-{
-public:
-    float BAR_UNIT::calculatePressureAndTemperature(float temp, float pres, int handle);
-    float BAR_UNIT::initialAMSL(float pres);
-    float BAR_UNIT::convertToAGL(float pres);
-private:
-    float initial_AMSL_; // Initial height above mean sea level when drone is on the ground
-    float height_AMSL_; // Height above mean sea level
-    float height_AGL_; // Height above ground level
-    uint32_t D1_, D2_; // Raw measurement data
-    uint16_t C1_, C2_, C3_, C4_, C5_, C6_; // Calibration data
-    float temp_; // Calculated temperature
-	float pres_; // Calculated pressure
-    int calibration_ = 0; // Calibration variable for getHeight()
-}
+#include "BAR_UNIT.hpp"
 
 /*Math constants*/
 #define p_0 101325 // Pressure at sea level
