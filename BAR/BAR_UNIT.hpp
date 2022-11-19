763d0733c69c9358e238954133370aff2b757c47
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-19 11:48:32 +0100

    Update BAR_UNIT.hpp

--- BAR/BAR_UNIT.hpp
+++ BAR/BAR_UNIT.hpp
@@ -3,16 +3,16 @@
 class BAR_UNIT
 {
 public:
-    float BAR_UNIT::calculatePressureAndTemperature(float temp, float pres, int handle);
+    //float BAR_UNIT::calculatePressureAndTemperature(float temp, float pres, int handle);
     float BAR_UNIT::initialAMSL(float pres);
     float BAR_UNIT::convertToAGL(float pres);
 private:
     float initial_AMSL_; // Initial height above mean sea level when drone is on the ground
     float height_AMSL_; // Height above mean sea level
     float height_AGL_; // Height above ground level
-    uint32_t D1_, D2_; // Raw measurement data
-    uint16_t C1_, C2_, C3_, C4_, C5_, C6_; // Calibration data
+    //uint32_t D1_, D2_; // Raw measurement data
+    //uint16_t C1_, C2_, C3_, C4_, C5_, C6_; // Calibration data
     float temp_; // Calculated temperature
 	float pres_; // Calculated pressure
-    int calibration_ = 0; // Calibration variable for getHeight()
+    //int calibration_ = 0; // Calibration variable for getHeight()
 }
