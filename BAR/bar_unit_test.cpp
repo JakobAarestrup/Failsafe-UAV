dc48fec00dbe1cb804d6802208bdb33523870d29
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-11-19 11:59:18 +0100

    Update bar_unit_test.cpp

--- BAR/bar_unit_test.cpp
+++ BAR/bar_unit_test.cpp
@@ -10,6 +10,8 @@ int main()
      */
     //float temperature[3] = {1,2,3};
     float pressure[4]  = {102532.096,101325,97772.574,95460.834}; //-100m, 0m, 300m, 500m
+    float initial_height;
+    float height;
 
     /**
      * @brief Temperature and Pressure calculation function
@@ -23,36 +25,36 @@ int main()
     /**
      * -100m to -100m
     */
-    float initial_height = b1.initialAMSL(pressure[0]);
-    float height = b1.convertToAGL(pressure[0]);
+    initial_height = b1.initialAMSL(pressure[0]);
+    height = b1.convertToAGL(pressure[0]);
     printf("-100m - -100m test: Initial Height: %f\nHeight: %f\n-------------------------------------\n",initial_height, height);
 
     /**
      * -100m to 0m
     */
-    float initial_height = b1.initialAMSL(pressure[0]);
-    float height = b1.convertToAGL(pressure[1]);
+    initial_height = b1.initialAMSL(pressure[0]);
+    height = b1.convertToAGL(pressure[1]);
     printf("-100m - 0m test: Initial Height: %f\nHeight: %f\n-------------------------------------\n",initial_height, height);
 
     /**
      * 0m to 300m
     */
-    float initial_height = b1.initialAMSL(pressure[1]);
-    float height = b1.convertToAGL(pressure[2]);
+    initial_height = b1.initialAMSL(pressure[1]);
+    height = b1.convertToAGL(pressure[2]);
     printf("0m - 300m test: Initial Height: %f\nHeight: %f\n-------------------------------------\n",initial_height, height);
 
     /**
      * 0m to 500m
     */
-    float initial_height = b1.initialAMSL(pressure[1]);
-    float height = b1.convertToAGL(pressure[3]);
+    initial_height = b1.initialAMSL(pressure[1]);
+    height = b1.convertToAGL(pressure[3]);
     printf("0m - 500m test: Initial Height: %f\nHeight: %f\n-------------------------------------\n",initial_height, height);
 
     /**
      * -100m to 500m
     */
-    float initial_height = b1.initialAMSL(pressure[0]);
-    float height = b1.convertToAGL(pressure[3]);
+    initial_height = b1.initialAMSL(pressure[0]);
+    height = b1.convertToAGL(pressure[3]);
     printf("-100m - 500m test: Initial Height: %f\nHeight: %f\n-------------------------------------\n",initial_height, height);
 
     return 0;
