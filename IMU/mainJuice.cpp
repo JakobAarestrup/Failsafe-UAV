cf62ca8dd7cd198940b770e8958c6d44d405e0e5
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-24 14:21:42 +0100

    Update mainJuice.cpp

--- IMU/mainJuice.cpp
+++ IMU/mainJuice.cpp
@@ -1,70 +1,99 @@
+#include "I2C.hpp"
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
-#include "IMU.hpp"
+#include <sys/ioctl.h>
+#include <fcntl.h>
 #include "LIS3MDL.hpp"
 #include "LSM6DSOX.hpp"
-#include <wiringPiI2C.h>
-#include <wiringPi.h>
-#include <stdbool.h>
-#include <sys/time.h>
-#include <time.h>
-#include "I2C.hpp"
-
-#define DT 0.02 // [s/loop] loop period. 20ms
-
-int mymillis()
+#include "I2Cdev.h"
+extern "C"
 {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+#include <linux/i2c-dev.h>
 }
 
-int main()
+// Constructor
+I2C::I2C()
 {
+}
 
-  I2C I1;
-  I2C I2;
-  IMU IMU1;
-  IMU IMU2;
+// Destructor
+I2C::~I2C()
+{
+  // delete [] I2C_Data_,I2C_File_,Write_File_; // Delete private variables
+}
 
-  int startofloop = mymillis();
-  struct timeval tvBegin, tvEnd, tvDiff;
-  gettimeofday(&tvBegin, NULL);
+// Open i2c communication
+void I2C::initializeI2C()
+{
+  /*  //First IMU:
+   I2Cdev::writeByte(LSM6DSOX_ADDR1, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
+   I2Cdev::writeByte(LSM6DSOX_ADDR1, LSM6DSOX_CTRL2_G, 0b10011100); // Gyro = 416 Hz (High-Performance mode)
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LSM6DSOX_CTRL1_XL, 0b011000000); // Acc = 416 Hz (High-Performance mode)
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LSM6DSOX_CTRL3_C, 0b01000000); // Enable BDU
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LSM6DSOX_CTRL7_G, 0b10000000); // Enable High-Performance mode for Gyro
 
-  I1.initializeI2C();
-  // I2.initializeI2C();
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LIS3MDL_CTRL_REG1, 0b01110000);// OM = 11 (ultra-high-performance mode for X and Y); DO = 100 (10 Hz ODR)
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LIS3MDL_CTRL_REG2, 0b00000000);// FS = 00 (+/- 4 gauss full scale)
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LIS3MDL_CTRL_REG3, 0b00000000);// MD = 00 (continuous-conversion mode)
+   I2Cdev::writeByte(LIS3MDL_ADDR1, LIS3MDL_CTRL_REG4, 0b00001100);// OMZ = 11 (ultra-high-performance mode for Z)
+*/
+  // Second IMU:
+  I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_INT1_CTRL, 0b00000011); // Enable gyroscope and accelerometer data interrupt
+  I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL2_G, 0b01001000);   // Gyro = 208 Hz (normal mode) 1000 dps
+  I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL1_XL, 0b011000000); // Acc = 416 Hz (High-Performance mode) 2g
+  I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL3_C, 0b01000000);   // Enable BDU
+  I2Cdev::writeByte(LSM6DSOX_ADDR2, LSM6DSOX_CTRL7_G, 0b01000000);   // enable HPF // default HPF (00) 16 Hz
+                                                                     // I2Cdev::writeByte(LIS3MDL_ADDR2, LSM6DSOX_CTRL7_G, 0b10000000); // Enable High-Performance mode for Gyro
 
-  /*Not used atm*/
-  IMU1.calibrateGyro(2);
-  // IMU2.calibrateGyro();
+  I2Cdev::writeByte(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG1, 0b01110000); // OM = 11 (ultra-high-performance mode for X and Y); DO = 100 (10 Hz ODR)
+  I2Cdev::writeByte(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG2, 0b00000000); // FS = 00 (+/- 4 gauss full scale)
+  I2Cdev::writeByte(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG3, 0b00000000); // MD = 00 (continuous-conversion mode)
+  I2Cdev::writeByte(LIS3MDL_ADDR2, LIS3MDL_CTRL_REG4, 0b00001100); // OMZ = 11 (ultra-high-performance mode for Z)
+}
 
-  // Main loop
-  while (1)
+// Write to I2C device register
+void I2C::writeI2C(int ADDR, int reg, int length, unsigned char *data)
+{
+  I2Cdev::writeBytes(ADDR, reg, length, data);
+}
+
+// Read from i2c device register
+float I2C::readI2C(int ADDR, int reg, int length, int HandleI2C)
+{
+  if (HandleI2C == 3) // 24-bit read from Barometer
   {
-    startofloop = mymillis();
-
-    IMU1.readIMU(2);
-
-    IMU1.ConvertACCData();
-    IMU1.ConvertMagData();
-
-    IMU1.ComplementaryFilter();
-
-    /* IMU1.readIMU();
-
-    IMU2.ConvertACCData();
-    IMU2.ConvertMagData();
-
-    IMU2.ComplementaryFilter();
- */
-
-    while (mymillis() - startofloop < 20)
-    {
-      usleep(100);
-    }
-
-    printf("Loop Time %d\n", mymillis() - startofloop);
+    uint8_t buff[3];
+    I2Cdev::readBytes(ADDR, reg, length, buff);
+    I2CData_ = (buff[0] << 16) | (buff[1] << 8) | buff[2]; // Konvertering af int til float
   }
-  return 0;
+  else if (HandleI2C == 2) // 16-bit read for Barometer calibration
+  {
+    uint8_t buff[2];
+    I2Cdev::readBytes(ADDR, reg, length, buff);
+    I2CData_ = buff[0] << 8 | buff[1]; // Konvertering af int til float
+  }
+  else if (HandleI2C == 1) // 16-bit read from IMU
+  {
+    uint8_t buff; // Read first register // Read second register
+    uint8_t nbuff;
+    I2Cdev::readBytes(ADDR, reg, length, &buff);
+    I2Cdev::readBytes(ADDR, reg + 1, length, &nbuff);
+    I2CData_ = (buff | nbuff << 8);
+
+    if (I2CData_ < 0x8000) // two's complement read from 16 bit register
+    {
+    }
+    else
+    {
+      I2CData_ = I2CData_ - 0xFFFF;
+    }
+  }
+  else // 8-bit read
+  {
+    uint8_t buff;
+    I2Cdev::readBytes(ADDR, reg, length, &buff);
+    I2CData_ = buff; // Convertering af int til float
+  }
+  return I2CData_;
 }
