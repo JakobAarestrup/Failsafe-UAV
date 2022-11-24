fda7c73c78a5d9b8507643bf83d5ec3fdc10aeb7
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-24 13:43:49 +0100

    Update IMU.cpp

--- RDS/ValidateState/IMU/IMU.cpp
+++ RDS/ValidateState/IMU/IMU.cpp
@@ -220,7 +220,7 @@ void IMU::readMAG(int IMU)
     mx = (mx / MG_Sensitivity) * 100;
     my = (my / MG_Sensitivity) * 100;
     mz = (mz / MG_Sensitivity) * 100;
-    float A[3][3];
+
     /**
      * @brief hardcoded offsets. b = Hard-Iron distortion and A = Soft-Iron distortion.
      *
@@ -231,9 +231,17 @@ void IMU::readMAG(int IMU)
         by = 3.137438;
         bz = -5.371644;
 
-        A[3][3] = {1.002979, 0.039343, -0.014713},
-        {0.039343, 1.019943, -0.006826},
-        {-0.014713, -0.006826, 1.014517};
+        A[3][3] = {{1.002979, 0.039343, -0.014713},
+                   {0.039343, 1.019943, -0.006826},
+                   {-0.014713, -0.006826, 1.014517}};
+
+        // formel magdataCalibrated = A(magdata-b)
+        mx = mx - bx;
+        my = my - by;
+        mz = mz - bz;
+
+        magCalibX_ = A[0][0] * mx + A[0][1] * my + A[0][2] * mz; // A[0,:]*(magdata-b)
+        magCalibY_ = A[1][0] * mx + A[1][1] * my + A[1][2] * mz; // A[1,:]*(magdata-b)
     }
     else if (IMU = 2) // TODO ny calibreringsdata
     {
@@ -241,18 +249,18 @@ void IMU::readMAG(int IMU)
         by = 3.137438;
         bz = -5.371644;
 
-        A[3][3] = {1.002979, 0.039343, -0.014713},
-        {0.039343, 1.019943, -0.006826},
-        {-0.014713, -0.006826, 1.014517};
+        A[3][3] = {{1.002979, 0.039343, -0.014713},
+                   {0.039343, 1.019943, -0.006826},
+                   {-0.014713, -0.006826, 1.014517}};
+
+        // formel magdataCalibrated = A(magdata-b)
+        mx = mx - bx;
+        my = my - by;
+        mz = mz - bz;
+
+        magCalibX_ = A[0][0] * mx + A[0][1] * my + A[0][2] * mz; // A[0,:]*(magdata-b)
+        magCalibY_ = A[1][0] * mx + A[1][1] * my + A[1][2] * mz; // A[1,:]*(magdata-b)
     }
-
-    // formel magdataCalibrated = A(magdata-b)
-    mx = mx - bx;
-    my = my - by;
-    mz = mz - bz;
-
-    magCalibX_ = A[0][0] * mx + A[0][1] * my + A[0][2] * mz; // A[0,:]*(magdata-b)
-    magCalibY_ = A[1][0] * mx + A[1][1] * my + A[1][2] * mz; // A[1,:]*(magdata-b)
 }
 
 void IMU::ConvertACCData()
