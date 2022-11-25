346b4a9494d38dae0ed281d09f1aa13ddf2ac56c
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-25 16:10:06 +0100

    Update

---
+++ BAR/barbar.cpp
@@ -0,0 +1,20 @@
+//#include "I2C.hpp"
+#include "BAR.hpp"
+#include "MS5611.hpp"
+#include <unistd.h>
+#include <stdio.h>
+#include "I2C.hpp"
+
+int main()
+{
+    BAR barometer;
+    int calibration;
+    while (1)
+    {
+        barometer.calibrateBAR();
+        barometer.getHeight();
+        usleep(100000);
+    }
+
+    return 0;
+}
