000d4080ec34fc1fdf8a144e01cc7ab43ea9c713
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-25 16:12:38 +0100

    Update BAR.hpp

--- BAR/BAR.hpp
+++ BAR/BAR.hpp
@@ -8,11 +8,11 @@
 class BAR
 {
 public:
-    BAR(); // Constructor
-    ~BAR();// Destructor
-    void convertToAGL(); // Converts the bar data into height in AGL
-    void initialAMSL(); // Collects the initial AMSL value
-    void calibrateBAR(); // Calibrates the barometer
+    BAR();                // Constructor
+    ~BAR();               // Destructor
+    void convertToAGL();  // Converts the bar data into height in AGL
+    void initialAMSL();   // Collects the initial AMSL value
+    void calibrateBAR();  // Calibrates the barometer
     int getCalibration(); // Returns the calibration variable
     void readPressure();
     void readTemperature();
@@ -20,12 +20,12 @@ public:
     void update();
     float getHeight(); // Returns the height above ground level
 private:
-    float initial_AMSL_; // Initial height above mean sea level when drone is on the ground
-    float height_AMSL_; // Height above mean sea level
-    float height_AGL_; // Height above ground level
-    uint32_t D1_, D2_; // Raw measurement data
+    float initial_AMSL_;                   // Initial height above mean sea level when drone is on the ground
+    float height_AMSL_;                    // Height above mean sea level
+    float height_AGL_;                     // Height above ground level
+    uint32_t D1_, D2_;                     // Raw measurement data
     uint16_t C1_, C2_, C3_, C4_, C5_, C6_; // Calibration data
-    float temp_; // Calculated temperature
-	float pres_; // Calculated pressure
-    int calibration_ = 0; // Calibration variable for getHeight()
+    float temp_;                           // Calculated temperature
+    float pres_;                           // Calculated pressure
+    int calibration_ = 0;                  // Calibration variable for getHeight()
 };
