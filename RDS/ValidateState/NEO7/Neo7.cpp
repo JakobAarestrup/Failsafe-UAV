01128fa0e655b804f39bfd20b836246b24e20e04
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-26 22:52:57 +0100

    Update Neo7.cpp

--- RDS/ValidateState/NEO7/Neo7.cpp
+++ RDS/ValidateState/NEO7/Neo7.cpp
@@ -165,7 +165,7 @@ void GPS::convertData() // converts GPS serial data to decimal degrees
 
     if (strcmp(NS, "") == 0 | strcmp(EW, "") == 0) // is 1 of the arrays empty?
     {
-        // std::cout << "NS or EW returned N/A. Skipping conversion..." << std::endl;
+        std::cout << "NS or EW returned N/A. Skipping conversion..." << std::endl;
     }
 
     else
