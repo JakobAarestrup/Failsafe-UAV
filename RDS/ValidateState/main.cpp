993ee1eb0eaa9d65799f460aa452978f32631db5
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-26 04:33:17 +0100

    Update main.cpp

--- RDS/ValidateState/main.cpp
+++ RDS/ValidateState/main.cpp
@@ -65,12 +65,12 @@ int main()
      RDS.HeightControl(); // Checks for Failure for height
                           // N++; */
 
-    while (mymillis() - startofloop < 100)
-    {
-      // usleep(100);
-    }
+    /*    while (mymillis() - startofloop < 100)
+       {
+         // usleep(100);
+       }
 
-    printf("Loop Time %d\n", mymillis() - startofloop);
+       printf("Loop Time %d\n", mymillis() - startofloop); */
     usleep(200000);
     /*   if (N > 5)
       {
