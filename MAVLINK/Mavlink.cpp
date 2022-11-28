86bdecec710fd1b8e8fe8529ba00f77595df36e2
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-28 15:27:04 +0100

    Update Mavlink.cpp

--- MAVLINK/Mavlink.cpp
+++ MAVLINK/Mavlink.cpp
@@ -10,12 +10,15 @@ int main()
 {
 
     int fd;
+    int data;
     if ((fd = serialOpen("/dev/ttyS0", 9600)) < 0)
     {
         printf("Failed\n");
     }
     while (1)
     {
+        data = serialGetchar(fd);
+        printf("%c", data);
     }
     return 0;
 }
