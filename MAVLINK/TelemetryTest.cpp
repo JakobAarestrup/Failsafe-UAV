5ffa991cfb6814da4f70f1ce1cacc3c12ab37fbb
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-30 00:45:17 +0100

    sudo update

--- MAVLINK/TelemetryTest.cpp
+++ MAVLINK/TelemetryTest.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <../MAVSDK/src/mavsdk/core/include/mavsdk/mavsdk.h>
-#include <../MAVSDK/src/mavsdk/plugins/telemetry/include/plugins/telemetry/telemetry.h>
+#include <mavsdk/mavsdk.h>
 
 int main()
 {
@@ -13,20 +12,6 @@ int main()
     }
     // System got discovered.
     System system = mavsdk.systems()[0];
-
-    auto telemetry = Telemetry{system};
-
-    const Telemetry::Result set_rate_result = telemetry.set_rate_position(1.0);
-    if (set_rate_result != Telemetry::Result::Success)
-    {
-        // handle rate-setting failure (in this case print error)
-        std::cout << "Setting rate failed:" << set_rate_result << '\n';
-    }
-
-    telemetry.subscribe_position([](Telemetry::Position position)
-                                 { std::cout << "Altitude: " << position.relative_altitude_m << " m" << std::endl
-                                             << "Latitude: " << position.latitude_deg << std::endl
-                                             << "Longitude: " << position.longitude_deg << '\n'; });
 
     return 0;
 }
