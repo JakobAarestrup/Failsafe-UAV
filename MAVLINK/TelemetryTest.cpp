7bc1d81771593f52fc2b8eb8edd867f87b4ecaa1
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-30 01:20:49 +0100

    Update TelemetryTest.cpp

--- MAVLINK/TelemetryTest.cpp
+++ MAVLINK/TelemetryTest.cpp
@@ -5,14 +5,23 @@
 int main()
 {
     mavsdk::Mavsdk mavsdk;
-    mavsdk.add_udp_connection();
-    // Wait for the system to connect via heartbeat
+    // ConnectionResult conn_result = mavsdk.add_udp_connection();
+    //  Wait for the system to connect via heartbeat
     while (mavsdk.systems().size() == 0)
     {
         usleep(1000000);
     }
 
     // System got discovered.
-    mavsdk::System system = mavsdk.systems();
+    auto system = get_system(mavsdk);
+    /* // System got discovered.
+    System system = mavsdk.systems()[0]; */
     return 0;
 }
+
+/*  Mavsdk mavsdk;
+   ConnectionResult connection_result = mavsdk.add_udp_connection();
+
+   if (connection_result != ConnectionResult::Success) {
+       std::cerr << "Connection failed: " << connection_result << '\n';
+       return 1; */
