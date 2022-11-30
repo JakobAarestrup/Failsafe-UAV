e9bc08f2affa7b5989fea21e63b92573a6495de7
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-30 18:00:09 +0100

    Update IMU.cpp

--- RDS/ValidateState/IMU/IMU.cpp
+++ RDS/ValidateState/IMU/IMU.cpp
@@ -300,30 +300,30 @@ void IMU::ComplementaryFilter()
  */
 int IMU::freeFall(int IMU)
 {
-    int freeFall = 0;
+    /*  int freeFall = 0;
 
-    if (IMU == 1)
-    {
-        fall_ = digitalRead(int 28);
+     if (IMU == 1)
+     {
+         fall_ = digitalRead(int 28);
 
-        if (fall_ == 1)
-        {
-            I1.WriteI2C(LSM6DSOX_ADDR1, lSM6DSOX_WAKE_UP_DUR, 1, 0b00000000); // 0x00 - Set event duration (FF_DUR5 bit)
-            I1.WriteI2C(LSM6DSOX_ADDR1, lSM6DSOX_FREE_FALL, 1, 0b00110000);   // 0x33 - Set FF threshold (FF_THS[2:0] = 156mg)
-            freeFall = digitalRead(int 28);                                   // replace 1 with output of interrupt status on GPIO pin # for IMU1
-        }
-    }
-    else if (IMU == 2)
-    {
-        fall_ = digitalRead(int 29); // output of interrupt status on GPIO pin 21 for IMU2
-        if (fall_ == 1)
-        {
-            I1.WriteI2C(LSM6DSOX_ADDR2, lSM6DSOX_WAKE_UP_DUR, 1, 0b00000000); // 0x00 - Set event duration (FF_DUR5 bit)
-            I1.WriteI2C(LSM6DSOX_ADDR2, lSM6DSOX_FREE_FALL, 1, 0b00110000);   // 0x33 - Set FF threshold (FF_THS[2:0] = 156mg)
-            freeFall = digitalRead(int 29);                                   // output of interrupt status on GPIO pin 21 for IMU2
-        }
-    }
-    return freeFall;
+         if (fall_ == 1)
+         {
+             I1.WriteI2C(LSM6DSOX_ADDR1, lSM6DSOX_WAKE_UP_DUR, 1, 0b00000000); // 0x00 - Set event duration (FF_DUR5 bit)
+             I1.WriteI2C(LSM6DSOX_ADDR1, lSM6DSOX_FREE_FALL, 1, 0b00110000);   // 0x33 - Set FF threshold (FF_THS[2:0] = 156mg)
+             freeFall = digitalRead(int 28);                                   // replace 1 with output of interrupt status on GPIO pin # for IMU1
+         }
+     }
+     else if (IMU == 2)
+     {
+         fall_ = digitalRead(int 29); // output of interrupt status on GPIO pin 21 for IMU2
+         if (fall_ == 1)
+         {
+             I1.WriteI2C(LSM6DSOX_ADDR2, lSM6DSOX_WAKE_UP_DUR, 1, 0b00000000); // 0x00 - Set event duration (FF_DUR5 bit)
+             I1.WriteI2C(LSM6DSOX_ADDR2, lSM6DSOX_FREE_FALL, 1, 0b00110000);   // 0x33 - Set FF threshold (FF_THS[2:0] = 156mg)
+             freeFall = digitalRead(int 29);                                   // output of interrupt status on GPIO pin 21 for IMU2
+         }
+     }
+     return freeFall; */
 }
 
 float IMU::getRoll() const
