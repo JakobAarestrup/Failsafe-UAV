eb5d38b580178bc84facc6c2ede8dc5656eb3a4d
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-11-30 01:45:54 +0100

    Update TelemetryTest.cpp

--- MAVLINK/TelemetryTest.cpp
+++ MAVLINK/TelemetryTest.cpp
@@ -6,40 +6,6 @@
 #include <future>
 
 using namespace mavsdk;
-using std::chrono::seconds;
-using std::this_thread::sleep_for;
-
-std::shared_ptr<System> get_system(Mavsdk &mavsdk)
-{
-    std::cout << "Waiting to discover system...\n";
-    auto prom = std::promise<std::shared_ptr<System>>{};
-    auto fut = prom.get_future();
-
-    // We wait for new systems to be discovered, once we find one that has an
-    // autopilot, we decide to use it.
-    Mavsdk::NewSystemHandle handle = mavsdk.subscribe_on_new_system([&mavsdk, &prom, &handle]()
-                                                                    {
-        auto system = mavsdk.systems().back();
-
-        if (system->has_autopilot()) {
-            std::cout << "Discovered autopilot\n";
-
-            // Unsubscribe again as we only want to find one system.
-            mavsdk.unsubscribe_on_new_system(handle);
-            prom.set_value(system);
-        } });
-
-    // We usually receive heartbeats at 1Hz, therefore we should find a
-    // system after around 3 seconds max, surely.
-    if (fut.wait_for(seconds(3)) == std::future_status::timeout)
-    {
-        std::cerr << "No autopilot found.\n";
-        return {};
-    }
-
-    // Get discovered system now.
-    return fut.get();
-}
 
 int main()
 {
@@ -58,11 +24,11 @@ int main()
         usleep(1000000);
     }
 
-    /*    auto system = get_system(mavsdk);
-       if (!system)
-       {
-           return 1;
-       } */
+    auto system = mavsdk.systems().back();
+    if (!system)
+    {
+        return 1;
+    }
     // System got discovered.
     // mavsdk::System system = mavsdk.systems()[0];
     return 0;
