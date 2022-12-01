31cad4f0289dca5939da6a97dbe8c2cbc3992bb6
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-01 17:11:27 +0100

    Gamer

--- RDS/ValidateState/sussy.cpp
+++ RDS/ValidateState/sussy.cpp
@@ -104,7 +104,7 @@ int main(int argc, char **argv)
     /* cout << "You have entered " << argc
          << " arguments:"
          << "\n";
-  */
+    */
     for (int i = 0; i < argc; ++i)
     {
         cout << argv[i] << " plads: " << i << endl;
