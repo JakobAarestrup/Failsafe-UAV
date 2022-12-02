0851cacef2c2c6a46145601e8f47e4f6b2ca3657
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-02 12:24:53 +0100

    Update GetMAVLinkdata.cpp

--- MAVLINK/GetMAVLinkdata.cpp
+++ MAVLINK/GetMAVLinkdata.cpp
@@ -66,29 +66,29 @@ int main()
     auto telemetry = Telemetry{system};
     auto action = Action{system};
 
-    // We want to listen to the altitude of the drone at 1 Hz.
-    const auto set_rate_result = telemetry.set_rate_position(1.0);
-    if (set_rate_result != Telemetry::Result::Success)
-    {
-        std::cerr << "Setting rate failed: " << set_rate_result << '\n';
-        return 1;
-    }
-
-    /*  const auto set_rate_result1 = telemetry.set_rate_attitude_euler(1.0);
-     if (set_rate_result1 != Telemetry::Result::Success)
+    /*  // We want to listen to the altitude of the drone at 1 Hz.
+     const auto set_rate_result = telemetry.set_rate_position(1.0);
+     if (set_rate_result != Telemetry::Result::Success)
      {
-         std::cerr << "Setting rate failed: " << set_rate_result1 << '\n';
+         std::cerr << "Setting rate failed: " << set_rate_result << '\n';
          return 1;
      } */
 
-    // Set up callback to monitor altitude while the vehicle is in flight
+    const auto set_rate_result1 = telemetry.set_rate_attitude_euler(1.0);
+    if (set_rate_result1 != Telemetry::Result::Success)
+    {
+        std::cerr << "Setting rate failed: " << set_rate_result1 << '\n';
+        return 1;
+    }
+
+    /* // Set up callback to monitor altitude while the vehicle is in flight
     telemetry.subscribe_position([](Telemetry::Position position)
                                  { std::cout << "Altitude: " << position.relative_altitude_m << " m" << std::endl
                                              << "Latitude: " << position.latitude_deg << std::endl
                                              << "Longitude: " << position.longitude_deg << '\n'; });
-
-    /* telemetry.subscribe_attitude_euler([](Telemetry::EulerAngle euler)
-                                       { std::cout << "Euler: (" << euler.roll_deg << ", " << euler.pitch_deg << ", " << euler.yaw_deg << ")" << std::endl; });
  */
+    telemetry.subscribe_attitude_euler([](Telemetry::EulerAngle euler)
+                                       { std::cout << "Euler: (" << euler.roll_deg << ", " << euler.pitch_deg << ", " << euler.yaw_deg << ")" << std::endl; });
+
     return 0;
 }
