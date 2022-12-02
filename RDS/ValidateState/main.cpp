aa4d52ba9d44b4c947679f88964e585ef80580d7
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-02 11:19:07 +0100

    Update main.cpp

--- RDS/ValidateState/main.cpp
+++ RDS/ValidateState/main.cpp
@@ -89,15 +89,15 @@ int main(int argc, char **argv)
   /* cout << "You have entered " << argc
        << " arguments:"
        << "\n";
-*/
+  */
   for (int i = 0; i < argc; ++i)
   {
-    cout << argv[i] << " plads: " << i << endl;
+    std::cout << argv[i] << " plads: " << i << std::endl;
   }
-  cout << "\n";
-  cout << argv[1] << "\n";
-  cout << argv[4] << "\n"
-       << endl;
+  std::cout << "\n";
+  std::cout << argv[1] << "\n";
+  std::cout << argv[4] << "\n"
+            << std::endl;
 
   if ((strcmp(argv[1], argv[4]) == 0)) // if you write 20 on serial it
   {
@@ -107,7 +107,6 @@ int main(int argc, char **argv)
   {
     RDS.configValidateState(argv[2], argv[3], argv[4], argv[5]);
   }
-
   /**
    * @brief Initialize used object variables
    *
