1441c1694738fda0bc8931d542891a6d3737890f
JakobAarestrup <72966422+JakobAarestrup@users.noreply.github.com>
2022-12-03 20:13:47 +0100

    Update UDP.cpp

--- UDP/UDP.cpp
+++ UDP/UDP.cpp
@@ -1,41 +1,19 @@
-#include <stdio.h>
-#include <sys/socket.h>
+#include<stdio.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>	//inet_addr
 
-//Socket() creates a socket and return descriptor. 
-//AF_INET = adress family (IPV4). 
-//SOCK_DGRAM = Decides protocol (TCP/UDP) = UDP. 
-//0 = IP protocol
+int main(int argc , char *argv[]){
+	int socket_desc;
+	struct sockaddr_in server;
+	
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1) printf("Could not create socket");
+			
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(20001);
 
-int main(int argc, char &argv[]){
-    int socket_desc;
-    socket_desc = socket(AF_INET, SOCK_DGRAM, 0); 
-    if (socket_desc == -1) printf("Could not create socket.");
-    return 0;
-
-    struct sockaddr_in server;
-
-    struct sockaddr_in 
-    {
-        short            sin_family;   // e.g. AF_INET, AF_INET6
-        unsigned short   sin_port;     // e.g. htons(3490)
-        struct in_addr   sin_addr;     // see struct in_addr, below
-        char             sin_zero[8];  // zero this if you want to
-    };
-
-    struct in_addr 
-    {
-        unsigned long s_addr;          // load with inet_pton()
-    };
-
-    struct sockaddr 
-    {
-        unsigned short    sa_family;    // address family, AF_xxx
-        char              sa_data[14];  // 14 bytes of protocol address
-    };
-
-    server.sin_addr.s_addr = inet_addr("74.125.235.20"); // IP is the address of google.com
-
-    
 	//Connect to remote server
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
 		puts("connect error");
