2daf985c3cb23fd04bd3d5865bac6999d2275e53
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-04 14:59:30 +0100

    Update Neo7.cpp

--- GPS/SOFTGPS/Neo7.cpp
+++ GPS/SOFTGPS/Neo7.cpp
@@ -176,6 +176,7 @@ void GPS::convertData() // converts GPS serial data to decimal degrees
 
     getNorthSouth(NS);
     getNorthSouth(EW);
+    print("EW: %c", EW);
 
     if ((strcmp(NS, "") == 0) | (strcmp(EW, "") == 0)) // is 1 of the arrays empty?
     {
