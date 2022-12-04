340e920721fd5b2f43355947cc2b1a25d5a932c1
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-04 14:33:50 +0100

    Update Neo7.cpp

--- GPS/SOFTGPS/Neo7.cpp
+++ GPS/SOFTGPS/Neo7.cpp
@@ -86,7 +86,6 @@ void GPS::readGPS() // reads GPS serial data
         memset(buff, 0, 255);
         len = read(serialPort_, buff, 255);
         printf("%s|", buff);
-        GPS_Data_ = buff[0];
         // GPS_Data_ = serialGetchar(serialPort_); /* receive character serially */
         //  printf("%c", GPS_Data_);
         //      read(serialPort_, &GPS_Data, 1);
