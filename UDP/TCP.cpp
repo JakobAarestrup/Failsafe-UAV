da041f9f3ff87b3c11b30bdad78fd082703573cf
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-05 19:19:11 +0100

    Update TCP.cpp

--- UDP/TCP.cpp
+++ UDP/TCP.cpp
@@ -1,80 +1,51 @@
-#include <unistd.h>
+// Client side implementation of UDP client-server model
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h> //inet_addr
-#include <string.h>    //strlen
-#include <fcntl.h>
-#include <cerrno>
-#include <poll.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-#define BUFLEN 100 // Max length of buffer
-#define PORT 42069 // The port on which to listen for incoming data
+#define PORT 42096
+#define MAXLINE 1024
 
-void die(char *s)
+// Driver code
+int main()
 {
-    perror(s);
-    exit(1);
-}
+    int sockfd;
+    char buffer[MAXLINE];
+    char *hello = "Hello from client";
+    struct sockaddr_in servaddr;
 
-int acceptSocket;
-char buf[BUFLEN];
-long sentbytes;
-
-int CreateSocket()
-{
-    struct sockaddr_in si_me, si_other, si_other2;
-
-    int s, i, slen = sizeof(si_other), recv_len;
-
-    // create a UDP socket
-    if ((acceptSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    // Creating socket file descriptor
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
-        die("socket");
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // zero out the structure
-    memset((char *)&si_me, 0, sizeof(si_me));
+    memset(&servaddr, 0, sizeof(servaddr));
 
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
 
-    // bind socket to port
-    if (bind(acceptSocket, (struct sockaddr *)&si_me, sizeof(si_me)) == -1)
-    {
-        die("bind");
-    }
-    char *message = "Hello from RDS";
-    // keep listening for data
-    while (1)
-    {
-        printf("Waiting for data...");
-        fflush(stdout);
+    int n, len;
 
-        // Send some data
-        if (send(acceptSocket) message, strlen(message), 0) < 0)
-            {
-                puts("Send failed\n");
-                return;
-            }
-        puts("Data Send\n");
+    sendto(sockfd, (const char *)hello, strlen(hello),
+           MSG_CONFIRM, (const struct sockaddr *)&servaddr,
+           sizeof(servaddr));
+    printf("Hello message sent.\n");
 
-        // try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(acceptSocket, buf, BUFLEN, 0, (struct sockaddr *)&si_other, &slen)) == -1) // read datagram from server socket
-        {
-            die("recvfrom()");
-        }
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                 MSG_WAITALL, (struct sockaddr *)&servaddr,
+                 &len);
+    buffer[n] = '\0';
+    printf("Server : %s\n", buffer);
 
-        // print details of the client/peer and the data received
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n", buf);
-
-        XcpIp_RxCallback((uint16)rc, (uint8 *)buf, (uint16)port);
-
-        makeTimer("First Timer", &firstTimerID, 2, 2);     // 2ms
-        makeTimer("Second Timer", &secondTimerID, 10, 10); // 10ms
-        makeTimer("Third Timer", &thirdTimerID, 100, 100); // 100ms
-    }
-
-    close(s);
+    close(sockfd);
+    return 0;
 }
