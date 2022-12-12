3b04bd6ffe412b57bbfb558c1c4980bdc6b0d87e
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-12 13:46:36 +0100

    update

--- RDS/IMU/IMU.hpp
+++ RDS/IMU/IMU.hpp
@@ -27,44 +27,44 @@ public:
 
 private:
     /// @brief Accelerometer calibrated x-axis in deg/s
-    float accCalibX_;
+    float accCalibX_ = 0.0;
     /// @brief Accelerometer calibrated y-axis in deg/s
-    float accCalibY_;
+    float accCalibY_ = 0.0;
     /// @brief Accelerometer calibrated z-axis in deg/s
-    float accCalibZ_;
+    float accCalibZ_ = 0.0;
 
     /// @brief Gyroscope calibrated x-axis in deg/s
-    float gyroCalibX_;
+    float gyroCalibX_ = 0.0;
     /// @brief Gyroscope calibrated y-axis in deg/s
-    float gyroCalibY_;
+    float gyroCalibY_ = 0.0;
     /// @brief Gyroscope calibrated z-axis in deg/s
-    float gyroCalibZ_;
+    float gyroCalibZ_ = 0.0;
 
     /// @brief Gyroscope drift in x-axis in
-    float gx_drift_;
+    float gx_drift_ = 0.0;
     /// @brief Gyroscope drift in y-axis in
-    float gy_drift_;
+    float gy_drift_ = 0.0;
     /// @brief Gyroscope drift in z-axis in
-    float gz_drift_;
+    float gz_drift_ = 0.0;
 
     /// @brief Magnetometer calibrated x-axis in deg/s
-    float magCalibX_;
+    float magCalibX_ = 0.0;
     /// @brief Magnetometer calibrated y-axis in deg/s
-    float magCalibY_;
+    float magCalibY_ = 0.0;
 
     /// @brief Accelerometer roll
     float xlRoll_ = 0.0;
     /// @brief Accelerometer pitch
     float xlPitch_ = 0.0;
     /// @brief Magnetometer yaw
-    float magYaw_;
+    float magYaw_ = 0.0;
 
     /// @brief Compensated roll
-    float compRoll_;
+    float compRoll_ = 0.0;
     /// @brief Compensated pitch
-    float compPitch_;
+    float compPitch_ = 0.0;
     /// @brief Compensated yaw
-    float compYaw_;
+    float compYaw_ = 0.0;
 
     /// @brief Mutex object m_
     std::mutex m_;
