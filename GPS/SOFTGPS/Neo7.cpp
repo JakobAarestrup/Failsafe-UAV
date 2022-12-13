d048160fa1271395c130a2d8553cf25df5e2b13c
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-13 11:59:51 +0100

    Update Neo7.cpp

--- GPS/SOFTGPS/Neo7.cpp
+++ GPS/SOFTGPS/Neo7.cpp
@@ -16,7 +16,7 @@ GPS::~GPS() // destructor
 
 int GPS::openUART(int fd) // open UART serial port
 {
-    if ((fd = serialOpen("/dev/ttyAMA1", 9600)) < 0) // open serial port with set baudrate
+    if ((fd = serialOpen("/dev/ttyS0", 9600)) < 0) // open serial port with set baudrate
     {
         fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno)); // error handling
 
