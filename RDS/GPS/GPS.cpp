2c1453ff41cabfe66a90cb54ef9d81598ccf6b11
Fjolte <72967687+Fjolte@users.noreply.github.com>
2022-12-13 11:21:04 +0100

    Update GPS.cpp

--- RDS/GPS/GPS.cpp
+++ RDS/GPS/GPS.cpp
@@ -28,7 +28,7 @@ GPS::~GPS() // destructor
 int GPS::openUART(int fd)
 {
     /*OPEN UART*/
-    if ((serialPort_ = serialOpen("/dev/ttyS0", 4800)) < 0) // open serial port with set baudrate
+    if ((serialPort_ = serialOpen("/dev/ttyAMA1", 4800)) < 0) // open serial port with set baudrate
     {
         fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno)); // error handling
 
